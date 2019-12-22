9c3ff4b35335959455c561c8803dcd9308888b5f
Ryan Houdek <Sonicadvance1@gmail.com>
2019-12-21 22:58:31 -0800

    Fixes memory allocation bug in BitSet, adds BitSetView.
    We were allocated too much memory for the BitSet.
    Adds a BitSetView class that views a window in to the BitSet

--- Source/Common/BitSet.h
+++ Source/Common/BitSet.h
@@ -1,49 +1,92 @@
 #pragma once
 #include "Common/MathUtils.h"
+#include "LogManager.h"
 
+#include <cstring>
 #include <stdint.h>
 #include <stdlib.h>
 #include <type_traits>
 
 template<typename T>
 struct BitSet final {
-	using ElementType = T;
-	constexpr static size_t MinimumSize = sizeof(ElementType);
-	constexpr static size_t MinimumSizeBits = sizeof(ElementType) * 8;
+  using ElementType = T;
+  constexpr static size_t MinimumSize = sizeof(ElementType);
+  constexpr static size_t MinimumSizeBits = sizeof(ElementType) * 8;
 
-	ElementType *Memory;
-	void Allocate(size_t Elements) {
-		Memory = static_cast<ElementType*>(malloc(AlignUp(Elements / MinimumSize, MinimumSize) * MinimumSize));
-	}
-	void Realloc(size_t Elements) {
-		Memory = static_cast<ElementType*>(realloc(Memory, AlignUp(Elements / MinimumSize, MinimumSize) * MinimumSize));
-	}
-	void Free() {
-		free(Memory);
-		Memory = nullptr;
-	}
-	bool Get(T Element) {
-		return (Memory[Element / MinimumSizeBits] & (1 << (Element % MinimumSizeBits))) != 0;
-	}
-	void Set(T Element) {
-		Memory[Element / MinimumSizeBits] |= (1ULL << (Element % MinimumSizeBits));
-	}
-	void Clear(T Element) {
-		Memory[Element / MinimumSizeBits] &= (1ULL << (Element % MinimumSizeBits));
-	}
-	void MemClear(size_t Size) {
-		memset(Memory, 0, Size);
-	}
-	void MemSet(size_t Size) {
-		memset(Memory, 0xFF, Size);
-	}
+  ElementType *Memory;
+  void Allocate(size_t Elements) {
+    Memory = static_cast<ElementType*>(malloc(AlignUp(Elements / MinimumSizeBits, MinimumSizeBits) * MinimumSize));
+  }
+  void Realloc(size_t Elements) {
+    Memory = static_cast<ElementType*>(realloc(Memory, AlignUp(Elements / MinimumSizeBits, MinimumSizeBits) * MinimumSize));
+  }
+  void Free() {
+    free(Memory);
+    Memory = nullptr;
+  }
+  bool Get(T Element) {
+    return (Memory[Element / MinimumSizeBits] & (1 << (Element % MinimumSizeBits))) != 0;
+  }
+  void Set(T Element) {
+    Memory[Element / MinimumSizeBits] |= (1ULL << (Element % MinimumSizeBits));
+  }
+  void Clear(T Element) {
+    Memory[Element / MinimumSizeBits] &= (1ULL << (Element % MinimumSizeBits));
+  }
+  void MemClear(size_t Elements) {
+    memset(Memory, 0, AlignUp(Elements / MinimumSizeBits, MinimumSizeBits));
+  }
+  void MemSet(size_t Elements) {
+    memset(Memory, 0xFF, AlignUp(Elements / MinimumSizeBits, MinimumSizeBits));
+  }
 
-	// This very explicitly doesn't let you take an address
-	// Is only a getter
-	bool operator[](T Element) {
-		return Get(Element);
-	}
+  // This very explicitly doesn't let you take an address
+  // Is only a getter
+  bool operator[](T Element) {
+    return Get(Element);
+  }
+};
+
+template<typename T>
+struct BitSetView final {
+  using ElementType = T;
+  constexpr static size_t MinimumSize = sizeof(ElementType);
+  constexpr static size_t MinimumSizeBits = sizeof(ElementType) * 8;
+
+  ElementType *Memory;
+
+  void GetView(BitSet<T> &Set, uint64_t ElementOffset) {
+    LogMan::Throw::A((ElementOffset % MinimumSize) == 0,
+        "Bitset view offset needs to be aligned to size of backing element");
+    Memory = &Set.Memory[ElementOffset / MinimumSizeBits];
+  }
+
+  bool Get(T Element) {
+    return (Memory[Element / MinimumSizeBits] & (1 << (Element % MinimumSizeBits))) != 0;
+  }
+  void Set(T Element) {
+    Memory[Element / MinimumSizeBits] |= (1ULL << (Element % MinimumSizeBits));
+  }
+  void Clear(T Element) {
+    Memory[Element / MinimumSizeBits] &= (1ULL << (Element % MinimumSizeBits));
+  }
+  void MemClear(size_t Elements) {
+    memset(Memory, 0, AlignUp(Elements / MinimumSizeBits, MinimumSizeBits));
+  }
+  void MemSet(size_t Elements) {
+    memset(Memory, 0xFF, AlignUp(Elements / MinimumSizeBits, MinimumSizeBits));
+  }
+
+  // This very explicitly doesn't let you take an address
+  // Is only a getter
+  bool operator[](T Element) {
+    return Get(Element);
+  }
+
 };
 
 static_assert(sizeof(BitSet<uint32_t>) == sizeof(uintptr_t), "Needs to just be a pointer");
 static_assert(std::is_pod<BitSet<uint32_t>>::value, "Needs to POD");
+
+static_assert(sizeof(BitSetView<uint32_t>) == sizeof(uintptr_t), "Needs to just be a pointer");
+static_assert(std::is_pod<BitSetView<uint32_t>>::value, "Needs to POD");
