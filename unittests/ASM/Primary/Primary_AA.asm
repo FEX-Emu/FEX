c51341294b4f5320e17a9ca8925a7315160556d5
Ryan Houdek <Sonicadvance1@gmail.com>
2020-03-09 22:59:59 -0700

    Adds STOS unit tests
    Tests all sizes that STOS supports, with the DF going up AND down.

---
+++ unittests/ASM/Primary/Primary_AA.asm
@@ -0,0 +1,40 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0xF2F2F2F2F2F2F2F2",
+    "RDX": "0x0",
+    "RDI": "0xE0000018"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rdx, 0xe0000000
+
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 0], rax
+mov rax, 0x5152535455565758
+mov [rdx + 8 * 1], rax
+mov rax, 0x0
+mov [rdx + 8 * 2], rax
+mov [rdx + 8 * 3], rax
+
+lea rdi, [rdx + 8 * 2]
+
+cld
+mov rax, 0xF2
+stosb ; rdi <- al
+stosb
+stosb
+stosb
+
+stosb
+stosb
+stosb
+stosb
+
+mov rax, [rdx + 8 * 2]
+mov rdx, [rdx + 8 * 3]
+hlt
---
+++ unittests/ASM/Primary/Primary_AB_dword_REP.asm
@@ -0,0 +1,33 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0xF1F2F3F4F1F2F3F4",
+    "RDX": "0x0",
+    "RDI": "0xE0000018"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rdx, 0xe0000000
+
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 0], rax
+mov rax, 0x5152535455565758
+mov [rdx + 8 * 1], rax
+mov rax, 0x0
+mov [rdx + 8 * 2], rax
+mov [rdx + 8 * 3], rax
+
+lea rdi, [rdx + 8 * 2]
+
+cld
+mov rax, 0xF1F2F3F4
+mov rcx, 2
+rep stosd ; rdi <- eax
+
+mov rax, [rdx + 8 * 2]
+mov rdx, [rdx + 8 * 3]
+hlt
