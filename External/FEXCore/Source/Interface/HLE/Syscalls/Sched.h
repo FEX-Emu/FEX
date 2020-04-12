94b0ea4be3ef487666f591060eaf07d0948d4f01
Ryan Houdek <Sonicadvance1@gmail.com>
2020-04-12 12:40:47 -0700

    Fixes the getaffinity syscall
    This was implemented with the glibc API originally which was incorrect.
    The syscall API is just a simple bitmask of for the affinity of the
    number of CPU cores and a size check that the resulting structure is
    large enough to store the number of bits.

--- External/FEXCore/Source/Interface/HLE/Syscalls/Sched.h
+++ External/FEXCore/Source/Interface/HLE/Syscalls/Sched.h
@@ -10,6 +10,6 @@ namespace FEXCore::HLE {
   uint64_t Sched_Yield(FEXCore::Core::InternalThreadState *Thread);
   uint64_t Getpriority(FEXCore::Core::InternalThreadState *Thread, int which, int who);
   uint64_t Setpriority(FEXCore::Core::InternalThreadState *Thread, int which, int who, int prio);
-  uint64_t Sched_Setaffinity(FEXCore::Core::InternalThreadState *Thread, pid_t pid, size_t cpusetsize, const cpu_set_t *mask);
-  uint64_t Sched_Getaffinity(FEXCore::Core::InternalThreadState *Thread, pid_t pid, size_t cpusetsize, cpu_set_t *mask);
+  uint64_t Sched_Setaffinity(FEXCore::Core::InternalThreadState *Thread, pid_t pid, size_t cpusetsize, const unsigned long *mask);
+  uint64_t Sched_Getaffinity(FEXCore::Core::InternalThreadState *Thread, pid_t pid, size_t cpusetsize, unsigned long *mask);
 }
