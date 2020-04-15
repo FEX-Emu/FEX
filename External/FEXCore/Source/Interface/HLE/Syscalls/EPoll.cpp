12e8e63be7c786f5e3c28b9c76ab27802b611363
Ryan Houdek <Sonicadvance1@gmail.com>
2020-04-15 11:13:36 -0700

    Adds epoll_create syscall function

--- External/FEXCore/Source/Interface/HLE/Syscalls/EPoll.cpp
+++ External/FEXCore/Source/Interface/HLE/Syscalls/EPoll.cpp
@@ -8,6 +8,11 @@ struct InternalThreadState;
 }
 
 namespace FEXCore::HLE {
+  uint64_t EPoll_Create(FEXCore::Core::InternalThreadState *Thread, int flags) {
+    uint64_t Result = epoll_create1(flags);
+    SYSCALL_ERRNO();
+  }
+
   uint64_t EPoll_Ctl(FEXCore::Core::InternalThreadState *Thread, int epfd, int op, int fd, void *event) {
     uint64_t Result = epoll_ctl(epfd, op, fd, reinterpret_cast<struct epoll_event*>(event));
     SYSCALL_ERRNO();
--- External/FEXCore/Source/Interface/HLE/Syscalls/EPoll.h
+++ External/FEXCore/Source/Interface/HLE/Syscalls/EPoll.h
@@ -6,6 +6,7 @@ struct InternalThreadState;
 }
 
 namespace FEXCore::HLE {
+  uint64_t EPoll_Create(FEXCore::Core::InternalThreadState *Thread, int flags);
   uint64_t EPoll_Ctl(FEXCore::Core::InternalThreadState *Thread, int epfd, int op, int fd, void *event);
   uint64_t EPoll_Pwait(FEXCore::Core::InternalThreadState *Thread, int epfd, void *events, int maxevent, int timeout, const void* sigmask);
   uint64_t EPoll_Create1(FEXCore::Core::InternalThreadState *Thread, int flags);
