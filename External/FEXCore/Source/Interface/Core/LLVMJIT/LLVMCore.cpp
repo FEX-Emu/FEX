a8d3cd67ad60784de3c65cc225971f93778e07c3
David Miller <dmiller423@gmail.com>
2020-04-23 11:12:45 -0400

    Added include of <llvm/IR/IntrinsicsX86.h> to LLVMCore.cpp

--- External/FEXCore/Source/Interface/Core/LLVMJIT/LLVMCore.cpp
+++ External/FEXCore/Source/Interface/Core/LLVMJIT/LLVMCore.cpp
@@ -13,6 +13,7 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/IR/IntrinsicsX86.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/TargetSelect.h>
