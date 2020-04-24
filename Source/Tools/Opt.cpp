d93c66e5f6028717873a9663c76b90cee5db91ba
David Miller <dmiller423@gmail.com>
2020-04-24 19:35:59 -0400

    Added EnvLoader to fontend binaries, called prior to ArgLoader

--- Source/Tools/Opt.cpp
+++ Source/Tools/Opt.cpp
@@ -1,10 +1,12 @@
 #include "Common/ArgumentLoader.h"
+#include "Common/EnvironmentLoader.h"
 #include "Common/Config.h"
 
 #include <FEXCore/IR/IntrusiveIRList.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char **const envp) {
   FEX::Config::Init();
+  FEX::EnvLoader::Load(envp);
   FEX::ArgLoader::Load(argc, argv);
 
   FEX::Config::Shutdown();
