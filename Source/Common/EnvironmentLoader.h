d0001e8f63646df9b96421157000d4256f55a966
David Miller <dmiller423@gmail.com>
2020-04-24 16:19:41 -0400

    cleanup previous implementation cruft in header

--- Source/Common/EnvironmentLoader.h
+++ Source/Common/EnvironmentLoader.h
@@ -6,18 +6,7 @@
 
 
 namespace FEX::EnvLoader {
+
   void Load(char *const envp[]);
 
-
-
-#if 0
-  class EnvironmentLoader
-  {
-    virtual ~EnvironmentLoader(){}
-    EnvironmentLoader(const char *envp[] {nullptr});
-
-  private:
-    unordered_map<string, string> EnvMap;
-  };
-#endif
 }
