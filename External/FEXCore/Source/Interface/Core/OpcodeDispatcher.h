ad467c5b30c2a2423040dcfef6b4caf9a0049445
Ryan Houdek <Sonicadvance1@gmail.com>
2020-04-25 17:55:37 -0700

    Adds new vector float negate and float add horizontal IR ops

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.h
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.h
@@ -433,6 +433,12 @@ public:
   IRPair<IROp_VCMPGT> _VCMPGT(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0, OrderedNode *ssa1) {
     return _VCMPGT(ssa0, ssa1, RegisterSize, ElementSize);
   }
+  IRPair<IROp_VFAdd> _VFAdd(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0, OrderedNode *ssa1) {
+    return _VFAdd(ssa0, ssa1, RegisterSize, ElementSize);
+  }
+  IRPair<IROp_VFAddP> _VFAddP(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0, OrderedNode *ssa1) {
+    return _VFAddP(ssa0, ssa1, RegisterSize, ElementSize);
+  }
   IRPair<IROp_VFCMPEQ> _VFCMPEQ(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0, OrderedNode *ssa1) {
     return _VFCMPEQ(ssa0, ssa1, RegisterSize, ElementSize);
   }
@@ -504,6 +510,9 @@ public:
   }
   IRPair<IROp_VNeg> _VNeg(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0) {
     return _VNeg(ssa0, RegisterSize, ElementSize);
+  }
+  IRPair<IROp_VFNeg> _VFNeg(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0) {
+    return _VFNeg(ssa0, RegisterSize, ElementSize);
   }
   IRPair<IROp_VNot> _VNot(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0) {
     return _VNot(ssa0, RegisterSize, ElementSize);
