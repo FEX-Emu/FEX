6cf5c00fe94008bb05e954155c18765982d361ba
Ryan Houdek <Sonicadvance1@gmail.com>
2020-05-29 23:37:27 -0700

    Implements LODS unit tests

---
+++ unittests/ASM/Primary/Primary_AD_REP_word_down.asm
@@ -0,0 +1,30 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0x4546",
+    "RSI": "0xE0000000"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rdx, 0xe0000000
+
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 0], rax
+mov rax, 0x5152535455565758
+mov [rdx + 8 * 1], rax
+mov rax, 0x0
+mov [rdx + 8 * 2], rax
+mov [rdx + 8 * 3], rax
+
+lea rsi, [rdx + 8 * 2]
+
+std
+mov rax, 0xFF
+mov rcx, 8
+rep lodsw
+
+hlt
