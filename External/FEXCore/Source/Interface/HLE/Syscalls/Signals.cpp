7700a4d24b49f013d44af93e913c001442b80dbd
Ryan Houdek <Sonicadvance1@gmail.com>
2020-08-18 19:41:52 +0100

    Adds support for guest sigprocmask
    This was causing our SIGBUS handling to fail in FTL

--- External/FEXCore/Source/Interface/HLE/Syscalls/Signals.cpp
+++ External/FEXCore/Source/Interface/HLE/Syscalls/Signals.cpp
@@ -14,10 +14,8 @@ namespace FEXCore::HLE {
       return Thread->CTX->SignalDelegation.RegisterGuestSignalHandler(signum, act, oldact);
     });
 
-    REGISTER_SYSCALL_IMPL(rt_sigprocmask, [](FEXCore::Core::InternalThreadState *Thread, int how, const sigset_t *set, sigset_t *oldset) -> uint64_t {
-      // XXX: Pass through SignalDelegator
-      uint64_t Result = ::sigprocmask(how, set, oldset);
-      SYSCALL_ERRNO();
+    REGISTER_SYSCALL_IMPL(rt_sigprocmask, [](FEXCore::Core::InternalThreadState *Thread, int how, const uint64_t *set, uint64_t *oldset) -> uint64_t {
+      return Thread->CTX->SignalDelegation.GuestSigProcMask(how, set, oldset);
     });
 
     REGISTER_SYSCALL_IMPL(sigaltstack, [](FEXCore::Core::InternalThreadState *Thread, const stack_t *ss, stack_t *old_ss) -> uint64_t {
