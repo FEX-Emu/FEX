2fb68583a077993ef5f3bb298157233e917f2cfc
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-09-02 20:33:04 +0300

    IR: Add DeadFlagStoreElimination
    Mutlblock pass that eliminates flag stores that are never read.
    
    This is a sibling to RCLSE and will likely be absorbed by it when we add MB support there.

--- External/FEXCore/Source/Interface/IR/Passes.h
+++ External/FEXCore/Source/Interface/IR/Passes.h
@@ -8,6 +8,7 @@ FEXCore::IR::Pass* CreateConstProp();
 FEXCore::IR::Pass* CreateContextLoadStoreElimination();
 FEXCore::IR::Pass* CreateSyscallOptimization();
 FEXCore::IR::Pass* CreateDeadFlagCalculationEliminination();
+FEXCore::IR::Pass* CreateDeadFlagStoreElimination();
 FEXCore::IR::Pass* CreatePassDeadCodeElimination();
 FEXCore::IR::Pass* CreateIRCompaction();
 FEXCore::IR::RegisterAllocationPass* CreateRegisterAllocationPass();
