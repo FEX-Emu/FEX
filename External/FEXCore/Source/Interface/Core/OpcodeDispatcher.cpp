dd7f3ddef3b64eff598e5ad33688ffc46fc94d23
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-09-10 02:18:02 +0300

    SHR/ASR,imm: Fix CF calculation

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
@@ -4701,7 +4701,7 @@ void OpDispatchBuilder::GenerateFlags_SignShiftRightImmediate(FEXCore::X86Tables
   // CF
   {
     // Extract the last bit shifted in to CF
-    SetRFLAG<FEXCore::X86State::RFLAG_CF_LOC>(_Bfe(1, Shift, Src1));
+    SetRFLAG<FEXCore::X86State::RFLAG_CF_LOC>(_Bfe(1, Shift-1, Src1));
   }
 
   // PF
@@ -4750,7 +4750,7 @@ void OpDispatchBuilder::GenerateFlags_ShiftRightImmediate(FEXCore::X86Tables::De
   // CF
   {
     // Extract the last bit shifted in to CF
-    SetRFLAG<FEXCore::X86State::RFLAG_CF_LOC>(_Bfe(1, Shift, Src1));
+    SetRFLAG<FEXCore::X86State::RFLAG_CF_LOC>(_Bfe(1, Shift-1, Src1));
   }
 
   // PF
