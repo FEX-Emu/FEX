83ac23be215d06d1a54d6325ec16e1054bb8571b
Ryan Houdek <Sonicadvance1@gmail.com>
2020-09-25 23:00:58 -0700

    Implements new VTBL1 IR op
    This perfectly matches the AArch64 variant and almost perfectly matches
    x86's pshufb instruction

--- External/FEXCore/include/FEXCore/IR/IREmitter.h
+++ External/FEXCore/include/FEXCore/IR/IREmitter.h
@@ -322,6 +322,9 @@ friend class FEXCore::IR::PassManager;
   IRPair<IROp_VUXTL2> _VUXTL2(uint8_t RegisterSize, uint8_t ElementSize, OrderedNode *ssa0) {
     return _VUXTL2(ssa0, RegisterSize, ElementSize);
   }
+  IRPair<IROp_VTBL1> _VTBL1(uint8_t RegisterSize, OrderedNode *ssa0, OrderedNode *ssa1) {
+    return _VTBL1(ssa0, ssa1, RegisterSize);
+  }
   IRPair<IROp_Jump> _Jump() {
     return _Jump(InvalidNode);
   }
