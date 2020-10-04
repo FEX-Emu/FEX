9c7cc2db1fe193c01192c6f800c983fe7ea2b283
Ryan Houdek <Sonicadvance1@gmail.com>
2020-10-04 16:09:41 -0700

    Implements unit test for MOVNTPD

---
+++ unittests/ASM/OpSize/66_2B.asm
@@ -0,0 +1,28 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "XMM0": ["0x4142434445464748", "0x5152535455565758"],
+    "XMM1": ["0x4142434445464748", "0x5152535455565758"]
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rdx, 0xe0000000
+
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 0], rax
+mov rax, 0x5152535455565758
+mov [rdx + 8 * 1], rax
+
+mov rax, 0x0
+mov [rdx + 8 * 2], rax
+mov [rdx + 8 * 3], rax
+
+movaps xmm0, [rdx + 8 * 0]
+movntpd [rdx + 8 * 2], xmm0
+movaps xmm1, [rdx + 8 * 2]
+
+hlt
