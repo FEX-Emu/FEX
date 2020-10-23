2a81468663b9455d57c0f90df487dc6f81bdab6f
Ryan Houdek <Sonicadvance1@gmail.com>
2020-10-22 18:52:20 -0700

    Adds a new test for push 8bit immediate
    On 64bit this will operate as a 64bit or a 16bit push, with the
    immediate being sign extended.
    
    We were only previously testing the 64bit variant

---
+++ unittests/ASM/Primary/Primary_6A_2.asm
@@ -0,0 +1,23 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0x000000000000FF81",
+    "RSP": "0xE0000018"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rsp, 0xe0000020
+
+push word 0
+push word 0
+push word 0
+push word -127
+
+mov rdx, 0xe0000020
+mov rax, [rdx - 8]
+
+hlt
