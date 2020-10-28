4b6914ad807a85726cea0e24dc8f547dd4627720
Ryan Houdek <Sonicadvance1@gmail.com>
2020-10-27 23:11:30 -0700

    Implements unit tests to test the stack popping
    Only tests the stack popping for FCOMP, FUCOMP, FCOMPP, and FUCOMPP.

---
+++ unittests/ASM/X87/DA_E9.asm
@@ -0,0 +1,17 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "MM6":  ["0x8000000000000000", "0x3FFF"],
+    "MM7":  ["0x8000000000000000", "0x3FFF"]
+  }
+}
+%endif
+
+; Only tests pop behaviour
+fld1
+fldz
+fldz
+fucompp
+fld1
+
+hlt
