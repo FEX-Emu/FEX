f6ef1fc8b5bfe8a1a42e4b694db86fde1786c18e
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-12-03 21:16:27 +0200

    Pop_Segments: pop cs is not a valid opcode

--- unittests/32Bit_ASM/Primary/Pop_Segments.asm
+++ unittests/32Bit_ASM/Primary/Pop_Segments.asm
@@ -2,7 +2,7 @@
 %ifdef CONFIG
 {
   "RegData": {
-    "RSP": "0xE000001C"
+    "RSP": "0xE0000040"
   },
   "Mode": "32BIT"
 }
@@ -16,9 +16,7 @@ push eax
 push eax
 push eax
 push eax
-push eax
 
-push ax
 push ax
 push ax
 push ax
@@ -28,14 +26,12 @@ push ax
 ; Only pops the segments
 ; Doesn't check for a correct segment value
 ; Just ensures we are popping the correct amount of data
-pop cs
 pop ss
 pop ds
 pop es
 pop fs
 pop gs
 
-o16 pop cs
 o16 pop ss
 o16 pop ds
 o16 pop es
