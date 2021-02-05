d3e98fedb47c5a5800059a26ca859736cfdbc0f5
Ryan Houdek <Sonicadvance1@gmail.com>
2021-02-05 10:03:23 -0800

    Pulls uname nodename from host system
    Hardcoding FEXCore as the nodename is an annoyance.
    Pull the host's nodename instead.
    
    Fixes #600

--- Source/Tests/LinuxSyscalls/x64/Info.cpp
+++ Source/Tests/LinuxSyscalls/x64/Info.cpp
@@ -9,15 +9,6 @@
 
 namespace FEX::HLE::x64 {
   void RegisterInfo() {
-    REGISTER_SYSCALL_IMPL_X64(uname, [](FEXCore::Core::InternalThreadState *Thread, struct utsname *buf) -> uint64_t {
-      strcpy(buf->sysname, "Linux");
-      strcpy(buf->nodename, "FEXCore");
-      strcpy(buf->release, "5.0.0");
-      strcpy(buf->version, "#" FEXCORE_VERSION);
-      strcpy(buf->machine, "x86_64");
-      return 0;
-    });
-
     REGISTER_SYSCALL_IMPL_X64(sysinfo, [](FEXCore::Core::InternalThreadState *Thread, struct sysinfo *info) -> uint64_t {
       uint64_t Result = ::sysinfo(info);
       SYSCALL_ERRNO();
