abb2e09c20193605b23414673cefe8688610f8f0
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2021-02-17 06:37:16 +0200

    AOTIR: Add support for 32-bit process

--- Source/Tests/LinuxSyscalls/x32/Memory.cpp
+++ Source/Tests/LinuxSyscalls/x32/Memory.cpp
@@ -1,5 +1,6 @@
 #include "Tests/LinuxSyscalls/Syscalls.h"
 #include "Tests/LinuxSyscalls/x32/Syscalls.h"
+#include <FEXCore/Debug/InternalThreadState.h>
 
 #include <bitset>
 #include <map>
@@ -7,23 +8,66 @@
 #include <stdint.h>
 #include <sys/mman.h>
 #include <sys/ipc.h>
+#include <unistd.h>
+
+static std::string get_fdpath(int fd)
+{
+    std::vector<char> buf(400);
+    ssize_t len;
+
+    std::string fdToName = "/proc/self/fd/" + std::to_string(fd);
+
+    do
+    {
+        buf.resize(buf.size() + 100);
+        len = ::readlink(fdToName.c_str(), &(buf[0]), buf.size());
+    } while (buf.size() == len);
+
+    if (len > 0)
+    {
+        buf[len] = '\0';
+        return (std::string(&(buf[0])));
+    }
+    /* handle error */
+    return "";
+}
 
 namespace FEX::HLE::x32 {
 
   void RegisterMemory() {
     REGISTER_SYSCALL_IMPL_X32(mmap, [](FEXCore::Core::InternalThreadState *Thread, uint32_t addr, uint32_t length, int prot, int flags, int fd, int32_t offset) -> uint64_t {
-      return (uint64_t)static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
+      auto Result = (uint64_t)static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
         mmap(reinterpret_cast<void*>(addr), length, prot,flags, fd, offset);
+
+      if (Result != -1 && !(flags & MAP_ANONYMOUS)) {
+        auto filename = get_fdpath(fd);
+
+        FEXCore::Context::AddNamedRegion(Thread->CTX, Result, length, offset, filename);
+      }
+
+      return Result;
     });
 
     REGISTER_SYSCALL_IMPL_X32(mmap2, [](FEXCore::Core::InternalThreadState *Thread, uint32_t addr, uint32_t length, int prot, int flags, int fd, uint32_t pgoffset) -> uint64_t {
-      return (uint64_t)static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
+      auto Result = (uint64_t)static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
         mmap(reinterpret_cast<void*>(addr), length, prot,flags, fd, (uint64_t)pgoffset * 0x1000);
+      
+      if (Result != -1 && !(flags & MAP_ANONYMOUS)) {
+        auto filename = get_fdpath(fd);
+
+        FEXCore::Context::AddNamedRegion(Thread->CTX, Result, length, pgoffset * 0x1000, filename);
+      }
+
+      return Result;
     });
 
     REGISTER_SYSCALL_IMPL_X32(munmap, [](FEXCore::Core::InternalThreadState *Thread, void *addr, size_t length) -> uint64_t {
-      return static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
+      auto Result = static_cast<FEX::HLE::x32::x32SyscallHandler*>(FEX::HLE::_SyscallHandler)->GetAllocator()->
         munmap(addr, length);
+      if (Result != -1) {
+        FEXCore::Context::RemoveNamedRegion(Thread->CTX, (uintptr_t)addr, length);
+      }
+      return Result;
     });
 
     REGISTER_SYSCALL_IMPL_X32(mprotect, [](FEXCore::Core::InternalThreadState *Thread, void *addr, uint32_t len, int prot) -> uint64_t {
