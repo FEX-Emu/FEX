8a39f4b25cbca36737332dac9c1d9007a0f63ad7
Ryan Houdek <Sonicadvance1@gmail.com>
2021-02-22 22:29:31 -0800

    Implements BTC, BTR, BTS atomic unit tests
    This just takes the regular non-atomic unit tests and changes them to
    have lock prefixes.
    These are all handled as byte sized atomics so there aren't any
    alignment problems.

---
+++ unittests/ASM/Secondary/08_XX_07_Atomic.asm
@@ -0,0 +1,61 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "R15": "0x1A"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+%macro cfmerge 0
+
+; Get CF
+sbb r14, r14
+and r14, 1
+
+; Merge in to results
+shl r15, 1
+or r15, r14
+
+%endmacro
+
+mov rdx, 0xe0000000
+
+mov rax, 0xFFFFFFFF80000000
+mov [rdx + 8 * 0], rax
+mov [rdx + 8 * 1], rax
+mov rax, 0x0
+mov [rdx + 8 * 2], rax
+mov rax, 0x01
+mov [rdx + 8 * 3], eax
+mov rax, 0x0
+mov [rdx + 8 * 3 + 4], eax
+
+xor r15, r15 ; Will contain our results
+
+; Test and set
+lock btc word [rdx], 1
+cfmerge
+
+; Ensure it is set
+bt word [rdx], 1
+cfmerge
+
+mov r13, 32
+lock btc dword [rdx], r13d
+cfmerge
+
+bt dword [rdx], r13d
+cfmerge
+
+lock btc qword [rdx], 64 * 2 + 63
+cfmerge
+
+bt qword [rdx], 64 * 2 + 63
+cfmerge
+
+hlt
+
+
