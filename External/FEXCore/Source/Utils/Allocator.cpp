d256bc59fd180a421b1d4d9cab5e56954ad9b332
Ryan Houdek <Sonicadvance1@gmail.com>
2021-04-20 18:11:10 -0700

    Override glibc allocator functions

--- External/FEXCore/Source/Utils/Allocator.cpp
+++ External/FEXCore/Source/Utils/Allocator.cpp
@@ -1,7 +1,23 @@
 #include <FEXCore/Utils/Allocator.h>
 #include <sys/mman.h>
 
+extern "C" {
+  extern void *__libc_malloc(size_t size);
+  extern void *__libc_realloc(void *ptr, size_t size);
+  extern void __libc_free(void *ptr);
+}
+
 namespace FEXCore::Allocator {
   MMAP_Hook mmap {::mmap};
   MUNMAP_Hook munmap {::munmap};
+  MALLOC_Hook malloc {::__libc_malloc};
+  REALLOC_Hook realloc {::__libc_realloc};
+  FREE_Hook free {::__libc_free};
+}
+
+extern "C" {
+  // Override the global functions
+  void *malloc(size_t size) { return FEXCore::Allocator::malloc(size); }
+  void *realloc(void *ptr, size_t size) { return FEXCore::Allocator::realloc(ptr, size); }
+  void free(void *ptr) { return FEXCore::Allocator::free(ptr); }
 }
--- External/FEXCore/include/FEXCore/Utils/Allocator.h
+++ External/FEXCore/include/FEXCore/Utils/Allocator.h
@@ -4,9 +4,16 @@
 #include <functional>
 
 namespace FEXCore::Allocator {
-  using MMAP_Hook = std::function<void*(void*, size_t, int, int, int, off_t)>;
-  using MUNMAP_Hook = std::function<int(void*, size_t)>;
+  using MMAP_Hook = void*(*)(void*, size_t, int, int, int, off_t);
+  using MUNMAP_Hook = int(*)(void*, size_t);
+
+  using MALLOC_Hook = void*(*)(size_t);
+  using REALLOC_Hook = void*(*)(void*, size_t);
+  using FREE_Hook = void(*)(void*);
 
   __attribute__((visibility("default"))) extern MMAP_Hook mmap;
   __attribute__((visibility("default"))) extern MUNMAP_Hook munmap;
+  __attribute__((visibility("default"))) extern MALLOC_Hook malloc;
+  __attribute__((visibility("default"))) extern REALLOC_Hook realloc;
+  __attribute__((visibility("default"))) extern FREE_Hook free;
 }
