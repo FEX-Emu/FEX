2d0dced9e79b037b2fd8adad0a898e6d78aa175a
Ryan Houdek <Sonicadvance1@gmail.com>
2021-04-28 16:14:18 -0700

    Work around old asound headers

--- Source/Tests/LinuxSyscalls/x32/Ioctl/asound.h
+++ Source/Tests/LinuxSyscalls/x32/Ioctl/asound.h
@@ -8,6 +8,14 @@
 namespace FEX::HLE::x32 {
 
 namespace asound {
+#ifndef SNDRV_TIMER_IOCTL_TREAD_OLD
+#define SNDRV_TIMER_IOCTL_TREAD_OLD	_IOW('T', 0x02, int)
+#endif
+
+#ifndef SNDRV_TIMER_IOCTL_TREAD64
+#define SNDRV_TIMER_IOCTL_TREAD64	_IOW('T', 0xa4, int)
+#endif
+
 #include "Tests/LinuxSyscalls/x32/Ioctl/asound.inl"
 }
 }
