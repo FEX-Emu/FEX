9498a41f9b8077ee2747663a7a018848036301ce
Ryan Houdek <Sonicadvance1@gmail.com>
2021-05-01 09:33:39 -0700

    More ioctl32 emulation
    More DRM and i915 things, then wireless for some reason renderdoc relies on it

--- Source/Tests/LinuxSyscalls/x32/Ioctl/drm.h
+++ Source/Tests/LinuxSyscalls/x32/Ioctl/drm.h
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 #include <drm/drm.h>
+#include <drm/drm_mode.h>
 #include <drm/i915_drm.h>
 #include <drm/amdgpu_drm.h>
 #include <drm/panfrost_drm.h>
@@ -12,6 +13,8 @@
 #include <drm/nouveau_drm.h>
 #include <sys/ioctl.h>
 
+#define CPYT(x) val.x = x
+#define CPYF(x) x = val.x
 namespace FEX::HLE::x32 {
 
 namespace DRM {
@@ -82,6 +85,587 @@ fex_drm_unique {
     unique_len = val.unique_len;
   }
 };
+
+struct
+__attribute__((annotate("alias-x86_32-drm_map")))
+__attribute__((annotate("fex-match")))
+fex_drm_map {
+	uint32_t offset;
+	uint32_t size;
+	enum drm_map_type type;
+	enum drm_map_flags flags;
+	compat_ptr<void> handle;
+	int32_t mtrr;
+
+  fex_drm_map() = delete;
+
+  operator drm_map() const {
+    drm_map val{};
+    CPYT(offset);
+    CPYT(size);
+    CPYT(type);
+    CPYT(flags);
+    CPYT(handle);
+    CPYT(mtrr);
+    return val;
+  }
+
+  fex_drm_map(struct drm_map val)
+    : handle { val.handle } {
+    CPYT(offset);
+    CPYT(size);
+    CPYT(type);
+    CPYT(flags);
+    CPYT(mtrr);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_client")))
+__attribute__((annotate("fex-match")))
+fex_drm_client {
+	int32_t idx;
+	int32_t auth;
+	uint32_t pid;
+	uint32_t uid;
+	uint32_t magic;
+	uint32_t iocs;
+
+  fex_drm_client() = delete;
+
+  operator drm_client() const {
+    drm_client val{};
+    CPYT(idx);
+    CPYT(auth);
+    CPYT(pid);
+    CPYT(uid);
+    CPYT(magic);
+    CPYT(iocs);
+    return val;
+  }
+
+  fex_drm_client(struct drm_client val) {
+    CPYF(idx);
+    CPYF(auth);
+    CPYF(pid);
+    CPYF(uid);
+    CPYF(magic);
+    CPYF(iocs);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_stats")))
+__attribute__((annotate("fex-match")))
+fex_drm_stats {
+	uint32_t count;
+	struct {
+		uint32_t value;
+		enum drm_stat_type type;
+	} data[15];
+
+  fex_drm_stats() = delete;
+
+  operator drm_stats() const {
+    drm_stats val{};
+    CPYT(count);
+    for (size_t i = 0; i < 15; ++i) {
+      CPYT(data[i].value);
+      CPYT(data[i].type);
+    }
+    return val;
+  }
+
+  fex_drm_stats(struct drm_stats val) {
+    CPYF(count);
+    for (size_t i = 0; i < 15; ++i) {
+      CPYF(data[i].value);
+      CPYF(data[i].type);
+    }
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_buf_desc")))
+__attribute__((annotate("fex-match")))
+fex_drm_buf_desc {
+	int32_t count;
+  int32_t size;
+	int32_t low_mark;
+	int32_t high_mark;
+	enum {
+		_DRM_PAGE_ALIGN = 0x01,
+		_DRM_AGP_BUFFER = 0x02,
+		_DRM_SG_BUFFER = 0x04,
+		_DRM_FB_BUFFER = 0x08,
+		_DRM_PCI_BUFFER_RO = 0x10
+	} flags;
+	uint32_t agp_start;
+
+  fex_drm_buf_desc() = delete;
+
+  operator drm_buf_desc() const {
+    drm_buf_desc val{};
+    CPYT(count);
+    CPYT(size);
+    CPYT(low_mark);
+    CPYT(high_mark);
+    CPYT(agp_start);
+    return val;
+  }
+
+  fex_drm_buf_desc(struct drm_buf_desc val) {
+    CPYF(count);
+    CPYF(size);
+    CPYF(low_mark);
+    CPYF(high_mark);
+    CPYF(agp_start);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_buf_info")))
+__attribute__((annotate("fex-match")))
+fex_drm_buf_info {
+	int32_t count;
+	compat_ptr<struct drm_buf_desc> list;
+
+  fex_drm_buf_info() = delete;
+
+  operator drm_buf_info() const {
+    drm_buf_info val{};
+    CPYT(count);
+    CPYT(list);
+    return val;
+  }
+
+  fex_drm_buf_info(struct drm_buf_info val)
+    : list { val.list } {
+    CPYF(count);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_buf_pub")))
+__attribute__((annotate("fex-match")))
+fex_drm_buf_pub {
+	int32_t idx;
+	int32_t total;
+	int32_t used;
+	compat_ptr<void> address;
+
+  fex_drm_buf_pub() = delete;
+
+  operator drm_buf_pub() const {
+    drm_buf_pub val{};
+    CPYT(idx);
+    CPYT(total);
+    CPYT(used);
+    CPYT(address);
+    return val;
+  }
+
+  fex_drm_buf_pub(struct drm_buf_pub val)
+    : address { val.address } {
+    CPYF(idx);
+    CPYF(total);
+    CPYF(used);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_buf_map")))
+__attribute__((annotate("fex-match")))
+fex_drm_buf_map {
+	int32_t count;
+#ifdef __cplusplus
+	compat_ptr<void> virt;
+#else
+	compat_ptr<void> virtual;
+#endif
+	compat_ptr<drm_buf_pub> list;
+
+  fex_drm_buf_map() = delete;
+
+  operator drm_buf_map() const {
+    drm_buf_map val{};
+    CPYT(count);
+#ifdef __cplusplus
+    CPYT(virt);
+#else
+    CPYT(virtual);
+#endif
+    CPYT(list);
+    return val;
+  }
+
+  fex_drm_buf_map(struct drm_buf_map val)
+#ifdef __cplusplus
+    : virt { val.virt }
+#else
+    : virtual { val.virtual }
+#endif
+    , list { val.list }
+  {
+    CPYF(count);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_buf_free")))
+__attribute__((annotate("fex-match")))
+fex_drm_buf_free {
+	int32_t count;
+	compat_ptr<int> list;
+
+  fex_drm_buf_free() = delete;
+
+  operator drm_buf_free() const {
+    drm_buf_free val{};
+    CPYT(count);
+    CPYT(list);
+    return val;
+  }
+
+  fex_drm_buf_free(struct drm_buf_free val)
+    : list { val.list } {
+    CPYF(count);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_ctx_priv_map")))
+__attribute__((annotate("fex-match")))
+fex_drm_ctx_priv_map {
+	uint32_t ctx_id;
+	compat_ptr<void> handle;
+
+  fex_drm_ctx_priv_map() = delete;
+
+  operator drm_ctx_priv_map() const {
+    drm_ctx_priv_map val{};
+    CPYT(ctx_id);
+    CPYT(handle);
+    return val;
+  }
+
+  fex_drm_ctx_priv_map(struct drm_ctx_priv_map val)
+    : handle { val.handle } {
+    CPYF(ctx_id);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_ctx_res")))
+__attribute__((annotate("fex-match")))
+fex_drm_ctx_res {
+	int32_t count;
+	compat_ptr<struct drm_ctx> contexts;
+
+  fex_drm_ctx_res() = delete;
+
+  operator drm_ctx_res() const {
+    drm_ctx_res val{};
+    CPYT(count);
+    return val;
+  }
+
+  fex_drm_ctx_res(struct drm_ctx_res val)
+    : contexts { val.contexts } {
+    CPYF(count);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_dma")))
+__attribute__((annotate("fex-match")))
+fex_drm_dma {
+	int32_t context;
+	int32_t send_count;
+	compat_ptr<int32_t> send_indices;
+	compat_ptr<int32_t> send_sizes;
+	enum drm_dma_flags flags;
+	int32_t request_count;
+	int32_t request_size;
+	compat_ptr<int32_t> request_indices;
+	compat_ptr<int32_t> request_sizes;
+	int32_t granted_count;
+
+  fex_drm_dma() = delete;
+
+  operator drm_dma() const {
+    drm_dma val{};
+    CPYT(context);
+    CPYT(send_count);
+    CPYT(send_indices);
+    CPYT(send_sizes);
+    CPYT(flags);
+    CPYT(request_count);
+    CPYT(request_size);
+    CPYT(request_indices);
+    CPYT(request_sizes);
+    CPYT(granted_count);
+    return val;
+  }
+
+  fex_drm_dma(struct drm_dma val)
+    : send_indices { val.send_indices }
+    , send_sizes { val.send_sizes }
+    , request_indices { val.request_indices }
+    , request_sizes { val.request_sizes } {
+    CPYF(context);
+    CPYF(send_count);
+    CPYF(flags);
+    CPYF(request_count);
+    CPYF(request_size);
+    CPYF(granted_count);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_scatter_gather")))
+__attribute__((annotate("fex-match")))
+fex_drm_scatter_gather {
+	uint32_t size;
+	uint32_t handle;
+
+  fex_drm_scatter_gather() = delete;
+
+  operator drm_scatter_gather() const {
+    drm_scatter_gather val{};
+    CPYT(size);
+    CPYT(handle);
+    return val;
+  }
+
+  fex_drm_scatter_gather(struct drm_scatter_gather val) {
+    CPYF(size);
+    CPYF(handle);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_wait_vblank_request")))
+__attribute__((annotate("fex-match")))
+fex_drm_wait_vblank_request {
+	enum drm_vblank_seq_type type;
+	uint32_t sequence;
+	uint32_t signal;
+
+  fex_drm_wait_vblank_request() = delete;
+
+  operator drm_wait_vblank_request() const {
+    drm_wait_vblank_request val{};
+    CPYT(type);
+    CPYT(sequence);
+    CPYT(signal);
+    return val;
+  }
+
+  fex_drm_wait_vblank_request(struct drm_wait_vblank_request val) {
+    CPYF(type);
+    CPYF(sequence);
+    CPYF(signal);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_wait_vblank_reply")))
+__attribute__((annotate("fex-match")))
+fex_drm_wait_vblank_reply {
+	enum drm_vblank_seq_type type;
+	uint32_t sequence;
+	int32_t tval_sec;
+	int32_t tval_usec;
+
+  fex_drm_wait_vblank_reply() = delete;
+
+  operator drm_wait_vblank_reply() const {
+    drm_wait_vblank_reply val{};
+    CPYT(type);
+    CPYT(sequence);
+    CPYT(tval_sec);
+    CPYT(tval_usec);
+    return val;
+  }
+
+  fex_drm_wait_vblank_reply(struct drm_wait_vblank_reply val) {
+    CPYF(type);
+    CPYF(sequence);
+    CPYF(tval_sec);
+    CPYF(tval_usec);
+  }
+};
+
+union
+__attribute__((annotate("alias-x86_32-drm_wait_vblank")))
+__attribute__((annotate("fex-match")))
+fex_drm_wait_vblank {
+	fex_drm_wait_vblank_request request;
+	fex_drm_wait_vblank_reply reply;
+
+  fex_drm_wait_vblank() = delete;
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_update_draw")))
+__attribute__((annotate("fex-match")))
+__attribute__((packed))
+fex_drm_update_draw {
+	drm_drawable_t handle;
+	uint32_t type;
+	uint32_t num;
+	compat_uint64_t data;
+
+  fex_drm_update_draw() = delete;
+
+  operator drm_update_draw() const {
+    drm_update_draw val{};
+    CPYT(handle);
+    CPYT(type);
+    CPYT(num);
+    CPYT(data);
+    return val;
+  }
+
+  fex_drm_update_draw(struct drm_update_draw val) {
+    CPYF(handle);
+    CPYF(type);
+    CPYF(num);
+    CPYF(data);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_mode_get_plane_res")))
+__attribute__((annotate("fex-match")))
+__attribute__((packed))
+fex_drm_mode_get_plane_res {
+	compat_uint64_t plane_id_ptr;
+	uint32_t count_planes;
+  fex_drm_mode_get_plane_res() = delete;
+
+  operator drm_mode_get_plane_res() const {
+    drm_mode_get_plane_res val{};
+    CPYT(plane_id_ptr);
+    CPYT(count_planes);
+    return val;
+  }
+
+  fex_drm_mode_get_plane_res(struct drm_mode_get_plane_res val) {
+    CPYF(plane_id_ptr);
+    CPYF(count_planes);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_mode_fb_cmd2")))
+__attribute__((annotate("fex-match")))
+__attribute__((packed))
+fex_drm_mode_fb_cmd2 {
+	uint32_t fb_id;
+	uint32_t width;
+	uint32_t height;
+	uint32_t pixel_format;
+	uint32_t flags;
+
+	uint32_t handles[4];
+	uint32_t pitches[4];
+	uint32_t offsets[4];
+	compat_uint64_t modifier[4];
+  fex_drm_mode_fb_cmd2() = delete;
+
+  operator drm_mode_fb_cmd2() const {
+    drm_mode_fb_cmd2 val{};
+    CPYT(fb_id);
+    CPYT(width);
+    CPYT(height);
+    CPYT(pixel_format);
+    CPYT(flags);
+    for (int i = 0; i < 4; ++i) {
+      CPYT(handles[i]);
+      CPYT(pitches[i]);
+      CPYT(offsets[i]);
+      CPYT(modifier[i]);
+    }
+    return val;
+  }
+
+  fex_drm_mode_fb_cmd2(struct drm_mode_fb_cmd2 val) {
+    CPYF(fb_id);
+    CPYF(width);
+    CPYF(height);
+    CPYF(pixel_format);
+    CPYF(flags);
+    for (int i = 0; i < 4; ++i) {
+      CPYF(handles[i]);
+      CPYF(pitches[i]);
+      CPYF(offsets[i]);
+      CPYF(modifier[i]);
+    }
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_mode_obj_get_properties")))
+__attribute__((annotate("fex-match")))
+__attribute__((packed))
+fex_drm_mode_obj_get_properties {
+	compat_uint64_t props_ptr;
+	compat_uint64_t prop_values_ptr;
+	uint32_t count_props;
+	uint32_t obj_id;
+	uint32_t obj_type;
+
+  fex_drm_mode_obj_get_properties() = delete;
+
+  operator drm_mode_obj_get_properties() const {
+    drm_mode_obj_get_properties val{};
+    val.props_ptr = props_ptr;
+    val.prop_values_ptr = prop_values_ptr;
+    val.count_props = count_props;
+    val.obj_id = obj_id;
+    val.obj_type = obj_type;
+    return val;
+  }
+
+  fex_drm_mode_obj_get_properties(struct drm_mode_obj_get_properties val) {
+    props_ptr = val.props_ptr;
+    prop_values_ptr = val.prop_values_ptr;
+    count_props = val.count_props;
+    obj_type = val.obj_type;
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_mode_obj_set_property")))
+__attribute__((annotate("fex-match")))
+__attribute__((packed))
+fex_drm_mode_obj_set_property {
+	compat_uint64_t value;
+	uint32_t prop_id;
+	uint32_t obj_id;
+	uint32_t obj_type;
+
+  fex_drm_mode_obj_set_property() = delete;
+
+  operator drm_mode_obj_set_property() const {
+    drm_mode_obj_set_property val{};
+    val.value = value;
+    val.prop_id = prop_id;
+    val.obj_id = obj_id;
+    val.obj_type = obj_type;
+    return val;
+  }
+
+  fex_drm_mode_obj_set_property(struct drm_mode_obj_set_property val) {
+    value = val.value;
+    prop_id = val.prop_id;
+    obj_id = val.obj_id;
+    obj_type = val.obj_type;
+  }
+};
+
 }
 
 namespace AMDGPU {
@@ -176,6 +760,144 @@ fex_drm_msm_wait_fence {
 
 namespace I915 {
 
+struct
+__attribute__((annotate("alias-x86_32-drm_i915_batchbuffer")))
+__attribute__((annotate("fex-match")))
+fex_drm_i915_batchbuffer_t {
+	int32_t start;
+	int32_t used;
+	int32_t DR1;
+	int32_t DR4;
+	int32_t num_cliprects;
+	compat_ptr<struct drm_clip_rect> cliprects;
+
+  fex_drm_i915_batchbuffer_t() = delete;
+
+  operator drm_i915_batchbuffer_t() const {
+    drm_i915_batchbuffer_t val{};
+    CPYT(start);
+    CPYT(used);
+    CPYT(DR1);
+    CPYT(DR4);
+    CPYT(num_cliprects);
+    CPYT(cliprects);
+    return val;
+  }
+
+  fex_drm_i915_batchbuffer_t(drm_i915_batchbuffer_t val)
+    : cliprects { val.cliprects } {
+    CPYF(start);
+    CPYF(used);
+    CPYF(DR1);
+    CPYF(DR4);
+    CPYF(num_cliprects);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_i915_irq_emit")))
+__attribute__((annotate("fex-match")))
+fex_drm_i915_irq_emit_t {
+	compat_ptr<int> irq_seq;
+
+  fex_drm_i915_irq_emit_t() = delete;
+
+  operator drm_i915_irq_emit_t() const {
+    drm_i915_irq_emit_t val{};
+    CPYT(irq_seq);
+    return val;
+  }
+
+  fex_drm_i915_irq_emit_t(drm_i915_irq_emit_t val)
+    : irq_seq { val.irq_seq } {
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_i915_getparam")))
+__attribute__((annotate("fex-match")))
+fex_drm_i915_getparam_t
+{
+	int32_t param;
+	compat_ptr<int> value;
+  fex_drm_i915_getparam_t() = delete;
+
+  operator drm_i915_getparam_t() const {
+    drm_i915_getparam_t val{};
+    CPYT(param);
+    CPYT(value);
+    return val;
+  }
+
+  fex_drm_i915_getparam_t(drm_i915_getparam_t val)
+    : value { val.value } {
+    CPYF(param);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-drm_i915_mem_alloc")))
+__attribute__((annotate("fex-match")))
+fex_drm_i915_mem_alloc_t
+{
+	int32_t region;
+	int32_t alignment;
+	int32_t size;
+	compat_ptr<int> region_offset;
+  fex_drm_i915_mem_alloc_t() = delete;
+
+  operator drm_i915_mem_alloc_t() const {
+    drm_i915_mem_alloc_t val{};
+    CPYT(region);
+    CPYT(alignment);
+    CPYT(size);
+    CPYT(region_offset);
+    return val;
+  }
+
+  fex_drm_i915_mem_alloc_t(drm_i915_mem_alloc_t val)
+    : region_offset { val.region_offset } {
+    CPYT(region);
+    CPYT(alignment);
+    CPYT(size);
+  }
+};
+
+struct
+__attribute__((annotate("alias-x86_32-_drm_i915_cmdbuffer")))
+__attribute__((annotate("fex-match")))
+fex_drm_i915_cmdbuffer_t
+{
+	compat_ptr<char> buf;
+	int32_t sz;
+	int32_t DR1;
+	int32_t DR4;
+	int32_t num_cliprects;
+	compat_ptr<struct drm_clip_rect> cliprects;
+
+  fex_drm_i915_cmdbuffer_t() = delete;
+
+  operator drm_i915_cmdbuffer_t() const {
+    drm_i915_cmdbuffer_t val{};
+    CPYT(buf);
+    CPYT(sz);
+    CPYT(DR1);
+    CPYT(DR4);
+    CPYT(num_cliprects);
+    CPYT(cliprects);
+    return val;
+  }
+
+  fex_drm_i915_cmdbuffer_t(drm_i915_cmdbuffer_t val)
+    : buf { val.buf }
+    , cliprects { val.cliprects } {
+    CPYT(sz);
+    CPYT(DR1);
+    CPYT(DR4);
+    CPYT(num_cliprects);
+  }
+};
+
 // I915 defines if they don't exist
 // Older DRM doesn't have this
 #ifndef DRM_IOCTL_I915_GEM_MMAP_OFFSET
@@ -199,4 +921,5 @@ struct drm_i915_gem_mmap_offset {
 #include "Tests/LinuxSyscalls/x32/Ioctl/nouveau_drm.inl"
 
 }
-
+#undef CPYT
+#undef CPYF
