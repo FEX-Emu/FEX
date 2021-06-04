b2ad8d73ac14b79f286866a9d398dcb9690a5217
Ryan Houdek <Sonicadvance1@gmail.com>
2021-06-04 06:04:39 -0700

    Adds unit test for FEX frontend decoder
    This could potentially show up as being ch instead of edi.
    Ensure we don't regress behaviour in the future

---
+++ unittests/ASM/H0F3A/66_20_1.asm
@@ -0,0 +1,23 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "XMM0": ["0xFFFFFFFFFFFF42FF", "0xFFFFFFFFFFFFFFFF"]
+  }
+}
+%endif
+
+mov rsi, 0xe0000000
+
+mov rax, -1
+mov [rsi + 8 * 0], rax
+mov [rsi + 8 * 1], rax
+
+movaps xmm0, [rsi + 16 * 0]
+
+mov rcx, 0
+mov edi, 0x42
+; This tests a frontend decoder bug in FEX
+; FEX thought this was ch
+pinsrb xmm0, edi, 0x01
+
+hlt
