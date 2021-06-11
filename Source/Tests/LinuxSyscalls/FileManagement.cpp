c971d3e56baf61a7d17de89d649337f0acb6e913
Lioncash <mathew1800@gmail.com>
2021-06-10 21:41:23 -0400

    FileManager: Construct fstream in place
    While we're at it, we can make use of .data() to avoid a bounds check
    that we know will never fail.

--- Source/Tests/LinuxSyscalls/FileManagement.cpp
+++ Source/Tests/LinuxSyscalls/FileManagement.cpp
@@ -31,41 +31,38 @@ $end_info$
 namespace FEX::HLE {
 
 static bool LoadFile(std::vector<char> &Data, const std::string &Filename) {
-  std::fstream File;
-  File.open(Filename, std::ios::in);
+  std::fstream File(Filename, std::ios::in);
 
   if (!File.is_open()) {
     return false;
   }
 
   if (!File.seekg(0, std::fstream::end)) {
-    LogMan::Msg::D("Couldn't load configuration file: Seek end");
+    LogMan::Msg::DFmt("Couldn't load configuration file: Seek end");
     return false;
   }
 
   auto FileSize = File.tellg();
   if (File.fail()) {
-    LogMan::Msg::D("Couldn't load configuration file: tellg");
+    LogMan::Msg::DFmt("Couldn't load configuration file: tellg");
     return false;
   }
 
   if (!File.seekg(0, std::fstream::beg)) {
-    LogMan::Msg::D("Couldn't load configuration file: Seek beginning");
+    LogMan::Msg::DFmt("Couldn't load configuration file: Seek beginning");
     return false;
   }
 
-  if (FileSize > 0) {
-    Data.resize(FileSize);
-    if (!File.read(&Data.at(0), FileSize)) {
-      // Probably means permissions aren't set. Just early exit
-      return false;
-    }
-    File.close();
-  }
-  else {
+  if (FileSize <= 0) {
+    LogMan::Msg::DFmt("FileSize less than or equal to zero specified");
     return false;
   }
 
+  Data.resize(FileSize);
+  if (!File.read(Data.data(), FileSize)) {
+    // Probably means permissions aren't set. Just early exit
+    return false;
+  }
   return true;
 }
 
