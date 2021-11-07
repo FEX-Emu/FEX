9df8245ef2f24dce2146ab8382e54dd226eb0a2c
lioncash <mathew1800@gmail.com>
2021-11-07 01:18:35 -0500

    OpcodeDispatcher: Implement BMI2 SARX/SHLX/SHRX
    Gets the basic BMI2 shifts out of the way.

---
+++ unittests/ASM/VEX/sarx.asm
@@ -0,0 +1,31 @@
+%ifdef CONFIG
+{
+  "RegData": {
+      "RAX": "0xF800000000000000",
+      "RBX": "4",
+      "RCX": "0xFFFFFFFFFFFFFFFF",
+      "RDX": "127",
+      "RSI": "63",
+      "RDI": "0x00000000FFFFFFFF"
+  }
+}
+%endif
+
+; Trivial right shift
+mov rax, 0x8000000000000000
+mov rbx, 4
+sarx rax, rax, rbx
+
+; This is really a shift by 63. This just ensures we properly
+; mask the shift value according to the ISA manual.
+mov rcx, 0x8000000000000000
+mov rdx, 127
+sarx rcx, rcx, rdx
+
+; This is really a shift by 31. This just ensures we properly
+; mask the shift value according to the ISA manual.
+mov edi, 0x80000000
+mov esi, 63
+sarx edi, edi, esi
+
+hlt
