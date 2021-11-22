6b6b5e880cdbdbc70ff156361d22d92594bfe8f3
Ryan Houdek <Sonicadvance1@gmail.com>
2021-11-22 13:33:11 -0800

    Linux: Bumps emulated Linux kernel version to 5.16
    Only if available as usual.
    
    Fixes #1159

--- Source/Tests/LinuxSyscalls/Syscalls.cpp
+++ Source/Tests/LinuxSyscalls/Syscalls.cpp
@@ -617,8 +617,8 @@ uint32_t SyscallHandler::CalculateHostKernelVersion() {
 }
 
 uint32_t SyscallHandler::CalculateGuestKernelVersion() {
-  // We currently only emulate a kernel between the ranges of Kernel 5.0.0 and 5.12.0
-  return std::max(KernelVersion(5, 0), std::min(KernelVersion(5, 12), GetHostKernelVersion()));
+  // We currently only emulate a kernel between the ranges of Kernel 5.0.0 and 5.16.0
+  return std::max(KernelVersion(5, 0), std::min(KernelVersion(5, 16), GetHostKernelVersion()));
 }
 
 uint64_t SyscallHandler::HandleSyscall(FEXCore::Core::CpuStateFrame *Frame, FEXCore::HLE::SyscallArguments *Args) {
