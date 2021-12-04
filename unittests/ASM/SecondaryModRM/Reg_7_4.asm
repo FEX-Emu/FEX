ed5d7f6a62e9227af4ce1385338608b23b966771
Ryan Houdek <Sonicadvance1@gmail.com>
2021-12-03 21:19:14 -0800

    unittests: Adds CLZero unit tests
    First test just ensures that an aligned cacheline clear will clear out
    the data.
    Second test ensures an unaligned address will still only clear the
    cacheline the address lies in without touching the other surrounding
    cachelines.
    
    Disabled on the host runner since the CI host runner doesn't support the
    instruction

---
+++ unittests/ASM/SecondaryModRM/Reg_7_4.asm
@@ -0,0 +1,36 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RBX": "0x0"
+  }
+}
+%endif
+
+; Starting address to store to
+mov rax, 0xe8000000
+
+; Set up the cacheline with garbage
+mov rbx, 0x4142434445464748
+mov [rax + 8 * 0], rbx
+mov [rax + 8 * 1], rbx
+mov [rax + 8 * 2], rbx
+mov [rax + 8 * 3], rbx
+mov [rax + 8 * 4], rbx
+mov [rax + 8 * 5], rbx
+mov [rax + 8 * 6], rbx
+mov [rax + 8 * 7], rbx
+
+clzero
+
+mov rbx, 0
+
+add rbx, [rax + 8 * 0]
+add rbx, [rax + 8 * 1]
+add rbx, [rax + 8 * 2]
+add rbx, [rax + 8 * 3]
+add rbx, [rax + 8 * 4]
+add rbx, [rax + 8 * 5]
+add rbx, [rax + 8 * 6]
+add rbx, [rax + 8 * 7]
+
+hlt
---
+++ unittests/ASM/SecondaryModRM/Reg_7_4_2.asm
@@ -0,0 +1,91 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RBX": "0x0",
+    "RCX": "0x000000020A121A20",
+    "RDX": "0x0A121A2000000000"
+  }
+}
+%endif
+
+; Starting address to store to
+mov rax, 0xe8000000
+
+; Set up the cachelines with garbage
+
+; Cacheline 0
+mov rbx, 0x0000000041424344
+mov [rax + 8 * 0], rbx
+mov [rax + 8 * 1], rbx
+mov [rax + 8 * 2], rbx
+mov [rax + 8 * 3], rbx
+mov [rax + 8 * 4], rbx
+mov [rax + 8 * 5], rbx
+mov [rax + 8 * 6], rbx
+mov [rax + 8 * 7], rbx
+
+; Cacheline 1
+mov rbx, 0x5152535455565758
+mov [rax + 8 * 8], rbx
+mov [rax + 8 * 9], rbx
+mov [rax + 8 * 10], rbx
+mov [rax + 8 * 11], rbx ; clzero here
+mov [rax + 8 * 12], rbx
+mov [rax + 8 * 13], rbx
+mov [rax + 8 * 14], rbx
+mov [rax + 8 * 15], rbx
+
+; Cacheline 2
+mov rbx, 0x4142434400000000
+mov [rax + 8 * 16], rbx
+mov [rax + 8 * 17], rbx
+mov [rax + 8 * 18], rbx
+mov [rax + 8 * 19], rbx
+mov [rax + 8 * 20], rbx
+mov [rax + 8 * 21], rbx
+mov [rax + 8 * 22], rbx
+mov [rax + 8 * 23], rbx
+
+; Set RAX to the middle of cacheline 1 to ensure alignment
+lea rax, [rax + 8 * 11]
+
+clzero
+
+; Set rax back to the start
+mov rax, 0xe8000000
+
+mov rbx, 0
+mov rcx, 0
+mov rdx, 0
+
+; Cacheline 0 should be unmodified
+add rcx, [rax + 8 * 0]
+add rcx, [rax + 8 * 1]
+add rcx, [rax + 8 * 2]
+add rcx, [rax + 8 * 3]
+add rcx, [rax + 8 * 4]
+add rcx, [rax + 8 * 5]
+add rcx, [rax + 8 * 6]
+add rcx, [rax + 8 * 7]
+
+; Cacheline 1 Should be zero
+add rbx, [rax + 8 * 8]
+add rbx, [rax + 8 * 9]
+add rbx, [rax + 8 * 10]
+add rbx, [rax + 8 * 11]
+add rbx, [rax + 8 * 12]
+add rbx, [rax + 8 * 13]
+add rbx, [rax + 8 * 14]
+add rbx, [rax + 8 * 15]
+
+; Cacheline 2 should be unmodified
+add rdx, [rax + 8 * 16]
+add rdx, [rax + 8 * 17]
+add rdx, [rax + 8 * 18]
+add rdx, [rax + 8 * 19]
+add rdx, [rax + 8 * 20]
+add rdx, [rax + 8 * 21]
+add rdx, [rax + 8 * 22]
+add rdx, [rax + 8 * 23]
+
+hlt
