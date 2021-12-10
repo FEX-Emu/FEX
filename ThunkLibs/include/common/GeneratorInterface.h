e3a2c8dc80a9ddff10e0005f6890c4464671a292
Tony Wasserka <neobrain@users.noreply.github.com>
2021-12-10 11:24:57 +0100

    Thunks: Use libclang-based code generation for libXfixes

---
+++ ThunkLibs/include/common/GeneratorInterface.h
@@ -0,0 +1,10 @@
+namespace fexgen {
+
+struct callback_annotation_base {
+    // Prevent annotating multiple callback strategies
+    bool prevent_multiple;
+};
+struct callback_stub : callback_annotation_base {};
+struct callback_guest : callback_annotation_base {};
+
+} // namespace fexgen
