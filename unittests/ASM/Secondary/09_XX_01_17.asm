a32c7f6ce2566e98462163e8e8a597c61551aeaf
Ryan Houdek <Sonicadvance1@gmail.com>
2022-03-05 16:39:40 -0800

    unittests: Adds cmpxchg unit tests for prefixes

---
+++ unittests/ASM/Secondary/09_XX_01_17.asm
@@ -0,0 +1,47 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0x4142434445464748",
+    "RDX": "0x5152535455565758",
+    "RBX": "0x6162636465666768",
+    "R15": "0x7172737475767778",
+    "R12": "0x4142434445464748",
+    "R13": "0x5152535455565758",
+    "R14": "0x0"
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+
+mov rcx, 0xe0000000
+
+mov rax, 0x4142434445464748
+mov [rcx + 8 * 0], rax
+mov rax, 0x5152535455565758
+mov [rcx + 8 * 1], rax
+
+mov r14, 0
+; Expected
+mov rax, 0x41424344FFFFFFFF
+mov rdx, 0x5152535455565758
+
+; Desired
+mov rbx, 0x6162636465666768
+mov r15, 0x7172737475767778
+
+; Prefix F3h, ensures it still operates at 16b
+db 0xF3
+cmpxchg16b [rcx]
+
+; Set r14 to 1 if if the memory location was expected
+setz r14b
+
+; rdx and rax will now contain the memory's data
+
+; Check memory location to ensure it contains what we want
+mov r12, [rcx + 8 * 0]
+mov r13, [rcx + 8 * 1]
+
+hlt
