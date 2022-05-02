9aaace51e1934505b3db78090c146c1dd65f6812
Tony Wasserka <neobrain@users.noreply.github.com>
2022-05-02 11:27:28 +0200

    ScopedSignalMask: Add usage guidelines

--- FEXHeaderUtils/FEXHeaderUtils/ScopedSignalMask.h
+++ FEXHeaderUtils/FEXHeaderUtils/ScopedSignalMask.h
@@ -12,7 +12,15 @@
 
 namespace FHU {
   /**
-   * @brief A class that masks signals and locks a mutex until it goes out of scope. It is NOT SAFE to move across threads.
+   * @brief A drop-in replacement for std::lock_guard that masks POSIX signals while the mutex is locked
+   *
+   * Use this class to prevent reentrancy issues of C++ mutexes with certain signal handlers.
+   * Common examples of such issues are:
+   * - C++ mutexes not unlocking due to a signal handler longjmping out of a scope owning the mutex
+   * - The signal handler itself using a mutex that would be re-locked if the handler gets invoked
+   *   again before unlocking
+   *
+   * Ownership of this object may be moved, but it is NOT SAFE to move across threads.
    *
    * Constructor order:
    * 1) Mask signals
@@ -21,8 +29,6 @@ namespace FHU {
    * Destructor Order:
    * 1) Unlock Mutex
    * 2) Unmask signals
-   *
-   * Masking signals around mutex locks is needed for signal-reentrant safety
    */
   template<typename MutexType, void (MutexType::*lock_fn)(), void (MutexType::*unlock_fn)()>
   class ScopedSignalMaskWithMutexBase final {
