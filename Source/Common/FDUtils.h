48a574dfd3043494e520868b7c3a4d29d5f1e07d
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2022-06-24 13:01:36 +0000

    FDUtils: Fix get_fdpath

--- Source/Common/FDUtils.h
+++ Source/Common/FDUtils.h
@@ -15,7 +15,7 @@ std::string get_fdpath(int fd) {
   std::filesystem::path Path = std::filesystem::path("/proc/self/fd") / std::to_string(fd);
   int Result = readlinkat(AT_FDCWD, Path.c_str(), SymlinkPath, sizeof(SymlinkPath));
   if (Result != -1) {
-    return SymlinkPath;
+    return std::string(SymlinkPath, Result);
   }
 
   LOGMAN_MSG_A_FMT("Couldn't get symlink from /proc/self/fd/{}", fd);
