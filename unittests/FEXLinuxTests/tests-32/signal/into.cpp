313ce0fed8e322351ba1a1e42d93c090d99098a7
Ryan Houdek <Sonicadvance1@gmail.com>
2022-06-30 14:35:41 -0700

    unittests: Adds 32-bit into test
    This is a very basic test to check for overflow exception.
    `into` instruction only exists on 32-bit, so it needs to not be built as
    a 64-bit executable.

---
+++ unittests/FEXLinuxTests/tests-32/signal/into.cpp
@@ -0,0 +1,36 @@
+#include <atomic>
+#include <signal.h>
+
+std::atomic<bool> CorrectFaultData{false};
+static void handler(int signal, siginfo_t *siginfo, void* context) {
+  ucontext_t *_context = (ucontext_t*)context;
+
+  if (signal != SIGSEGV) {
+    return;
+  }
+
+  if (siginfo->si_addr != nullptr) {
+    return;
+  }
+
+  if (_context->uc_mcontext.gregs[REG_TRAPNO] != 4) {
+    return;
+  }
+
+  CorrectFaultData = true;
+}
+
+int main() {
+  struct sigaction act{};
+  act.sa_sigaction = handler;
+  act.sa_flags = SA_SIGINFO;
+  sigaction(SIGSEGV, &act, nullptr);
+
+  __asm volatile(R"(
+  mov $0x7f, %%eax;
+  incb %%al;
+  into;
+  )" ::: "eax");
+
+  return CorrectFaultData ? 0 : 1;
+}
