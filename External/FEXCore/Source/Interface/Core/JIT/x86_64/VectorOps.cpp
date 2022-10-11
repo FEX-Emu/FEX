71043e372ad4d9a77e9b980b1ab53d20dab649b4
lioncash <mai.iam2048@gmail.com>
2022-10-11 23:48:07 +0000

    IR: Handle 256-bit VSMul/VUMul
    Extends VSMul and VUMul to handle 256-bit vectors.

--- External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
+++ External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
@@ -2412,17 +2412,26 @@ DEF_OP(VSQXTUN2) {
 }
 
 DEF_OP(VMul) {
-  auto Op = IROp->C<IR::IROp_VUMul>();
-  switch (Op->Header.ElementSize) {
+  const auto Op = IROp->C<IR::IROp_VUMul>();
+
+  const auto ElementSize = Op->Header.ElementSize;
+
+  const auto Dst = ToYMM(GetDst(Node));
+  const auto Vector1 = ToYMM(GetSrc(Op->Vector1.ID()));
+  const auto Vector2 = ToYMM(GetSrc(Op->Vector2.ID()));
+
+  switch (ElementSize) {
     case 2: {
-      vpmullw(GetDst(Node), GetSrc(Op->Vector1.ID()), GetSrc(Op->Vector2.ID()));
+      vpmullw(Dst, Vector1, Vector2);
       break;
     }
     case 4: {
-      vpmulld(GetDst(Node), GetSrc(Op->Vector1.ID()), GetSrc(Op->Vector2.ID()));
+      vpmulld(Dst, Vector1, Vector2);
       break;
     }
-    default: LOGMAN_MSG_A_FMT("Unknown Element Size: {}", Op->Header.ElementSize); break;
+    default:
+      LOGMAN_MSG_A_FMT("Unknown Element Size: {}", ElementSize);
+      break;
   }
 }
 
