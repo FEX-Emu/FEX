63dd09cd6fece60bd5dd399a838c49b7bfdab8ce
lioncash <mai.iam2048@gmail.com>
2022-11-16 17:56:37 +0000

    x86_64/VectorOps: Separate 128-bit VUQAdd path

--- External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
+++ External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
@@ -294,21 +294,31 @@ DEF_OP(VSub) {
 }
 
 DEF_OP(VUQAdd) {
-  auto Op = IROp->C<IR::IROp_VUQAdd>();
+  const auto Op = IROp->C<IR::IROp_VUQAdd>();
+  const auto OpSize = IROp->Size;
 
   const auto ElementSize = Op->Header.ElementSize;
+  const auto Is256Bit = OpSize == Core::CPUState::XMM_AVX_REG_SIZE;
 
-  const auto Dst = ToYMM(GetDst(Node));
-  const auto Vector1 = ToYMM(GetSrc(Op->Vector1.ID()));
-  const auto Vector2 = ToYMM(GetSrc(Op->Vector2.ID()));
+  const auto Dst = GetDst(Node);
+  const auto Vector1 = GetSrc(Op->Vector1.ID());
+  const auto Vector2 = GetSrc(Op->Vector2.ID());
 
   switch (ElementSize) {
     case 1: {
-      vpaddusb(Dst, Vector1, Vector2);
+      if (Is256Bit) {
+        vpaddusb(ToYMM(Dst), ToYMM(Vector1), ToYMM(Vector2));
+      } else {
+        vpaddusb(Dst, Vector1, Vector2);
+      }
       break;
     }
     case 2: {
-      vpaddusw(Dst, Vector1, Vector2);
+      if (Is256Bit) {
+        vpaddusw(ToYMM(Dst), ToYMM(Vector1), ToYMM(Vector2));
+      } else {
+        vpaddusw(Dst, Vector1, Vector2);
+      }
       break;
     }
     default:
