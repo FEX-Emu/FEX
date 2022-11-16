7e36473affac15f6b5d195a822b0cad48fda3a69
lioncash <mai.iam2048@gmail.com>
2022-11-16 19:19:28 +0000

    x86_64/VectorOps: Separate 128-bit VFCMPGT path

--- External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
+++ External/FEXCore/Source/Interface/Core/JIT/x86_64/VectorOps.cpp
@@ -2250,6 +2250,7 @@ DEF_OP(VFCMPGT) {
   const auto OpSize = IROp->Size;
 
   const auto ElementSize = Op->Header.ElementSize;
+  const auto Is256Bit = OpSize == Core::CPUState::XMM_AVX_REG_SIZE;
   const auto IsScalar = ElementSize == OpSize;
 
   const auto Dst = GetDst(Node);
@@ -2268,18 +2269,21 @@ DEF_OP(VFCMPGT) {
       LOGMAN_MSG_A_FMT("Unsupported element size: {}", ElementSize);
       break;
     }
-  }
-  else {
-    const auto DstYMM = ToYMM(Dst);
-    const auto Vector1YMM = ToYMM(Vector1);
-    const auto Vector2YMM = ToYMM(Vector2);
-
+  } else {
     switch (ElementSize) {
     case 4:
-      vcmpps(DstYMM, Vector2YMM, Vector1YMM, 1);
+      if (Is256Bit) {
+        vcmpps(ToYMM(Dst), ToYMM(Vector2), ToYMM(Vector1), 1);
+      } else {
+        vcmpps(Dst, Vector2, Vector1, 1);
+      }
       break;
     case 8:
-      vcmppd(DstYMM, Vector2YMM, Vector1YMM, 1);
+      if (Is256Bit) {
+        vcmppd(ToYMM(Dst), ToYMM(Vector2), ToYMM(Vector1), 1);
+      } else {
+        vcmppd(Dst, Vector2, Vector1, 1);
+      }
       break;
     default:
       LOGMAN_MSG_A_FMT("Unsupported element size: {}", ElementSize);
