3e6d23ae7ec140b3e04bb87174122bcf74b26b40
Ryan Houdek <Sonicadvance1@gmail.com>
2022-11-24 17:47:31 -0800

    unittests: SGDT tests

---
+++ unittests/32Bit_ASM/Secondary/07_XX_00.asm
@@ -0,0 +1,21 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0",
+    "RBX": "0x00000000FFFE0000"
+  },
+  "Mode": "32BIT"
+}
+%endif
+
+sgdt [rel data]
+
+movzx eax, word [rel data]
+mov ebx, dword [rel data + 2]
+hlt
+
+data:
+; Limit
+dw 0
+; Base
+dd 0
---
+++ unittests/ASM/Secondary/07_XX_00.asm
@@ -0,0 +1,20 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "0",
+    "RBX": "0xFFFFFFFFFFFE0000"
+  }
+}
+%endif
+
+sgdt [rel data]
+
+movzx rax, word [rel data]
+mov rbx, qword [rel data + 2]
+hlt
+
+data:
+; Limit
+dw 0
+; Base
+dq 0
