a483bc9837cf2baa3d1b6a085a039b69850f6a29
lioncash <mai.iam2048@gmail.com>
2022-11-30 15:44:25 +0000

    OpcodeDispatcher: Handle VANDNPD

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
@@ -5831,6 +5831,7 @@ void OpDispatchBuilder::InstallHostSpecificOpcodeHandlers() {
     {OPD(1, 0b01, 0x54), 1, &OpDispatchBuilder::AVXVectorALUOp<IR::OP_VAND, 16>},
 
     {OPD(1, 0b00, 0x55), 1, &OpDispatchBuilder::VANDNOp},
+    {OPD(1, 0b01, 0x55), 1, &OpDispatchBuilder::VANDNOp},
 
     {OPD(1, 0b00, 0x56), 1, &OpDispatchBuilder::AVXVectorALUOp<IR::OP_VOR, 16>},
     {OPD(1, 0b01, 0x56), 1, &OpDispatchBuilder::AVXVectorALUOp<IR::OP_VOR, 16>},
