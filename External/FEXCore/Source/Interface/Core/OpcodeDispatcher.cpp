25428cb28c8cc0629c7d6bcbea8bbdf32824e46e
lioncash <mai.iam2048@gmail.com>
2022-12-07 19:35:47 +0000

    OpcodeDispatcher: Handle VSQRTPD

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
@@ -5827,6 +5827,7 @@ void OpDispatchBuilder::InstallHostSpecificOpcodeHandlers() {
     {OPD(1, 0b01, 0x2F), 1, &OpDispatchBuilder::UCOMISxOp<8>},
 
     {OPD(1, 0b00, 0x51), 1, &OpDispatchBuilder::AVXVectorUnaryOp<IR::OP_VFSQRT, 4, false>},
+    {OPD(1, 0b01, 0x51), 1, &OpDispatchBuilder::AVXVectorUnaryOp<IR::OP_VFSQRT, 8, false>},
 
     {OPD(1, 0b00, 0x54), 1, &OpDispatchBuilder::AVXVectorALUOp<IR::OP_VAND, 16>},
     {OPD(1, 0b01, 0x54), 1, &OpDispatchBuilder::AVXVectorALUOp<IR::OP_VAND, 16>},
