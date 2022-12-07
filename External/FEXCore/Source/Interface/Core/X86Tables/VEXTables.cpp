83bccc00329a5f9b22273e1a92dc755caad65b69
lioncash <mai.iam2048@gmail.com>
2022-12-07 21:23:05 +0000

    OpcodeDispatcher: Handle VUCOMISD

--- External/FEXCore/Source/Interface/Core/X86Tables/VEXTables.cpp
+++ External/FEXCore/Source/Interface/Core/X86Tables/VEXTables.cpp
@@ -131,7 +131,7 @@ void InitializeVEXTables() {
     {OPD(1, 0b11, 0x2D), 1, X86InstInfo{"VCVTSD2SI",   TYPE_UNDEC, FLAGS_NONE, 0, nullptr}},
 
     {OPD(1, 0b00, 0x2E), 1, X86InstInfo{"VUCOMISS",   TYPE_INST, GenFlagsSizes(SIZE_128BIT, SIZE_32BIT) | FLAGS_MODRM | FLAGS_XMM_FLAGS, 0, nullptr}},
-    {OPD(1, 0b01, 0x2E), 1, X86InstInfo{"VUCOMISD",   TYPE_UNDEC, FLAGS_NONE, 0, nullptr}},
+    {OPD(1, 0b01, 0x2E), 1, X86InstInfo{"VUCOMISD",   TYPE_INST, GenFlagsSizes(SIZE_128BIT, SIZE_64BIT) | FLAGS_MODRM | FLAGS_XMM_FLAGS, 0, nullptr}},
 
     {OPD(1, 0b00, 0x2F), 1, X86InstInfo{"VCOMISS",   TYPE_UNDEC, FLAGS_NONE, 0, nullptr}},
     {OPD(1, 0b01, 0x2F), 1, X86InstInfo{"VCOMISD",   TYPE_UNDEC, FLAGS_NONE, 0, nullptr}},
