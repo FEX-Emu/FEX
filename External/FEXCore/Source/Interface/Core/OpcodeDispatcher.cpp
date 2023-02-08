63bf0d58266e918a7d969ffdd08e7136bb5a58e9
Lioncache <mai.iam2048@gmail.com>
2023-02-08 15:45:13 -0500

    OpcodeDispatcher: Handle VEXTRACTI128

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.cpp
@@ -6114,6 +6114,7 @@ void OpDispatchBuilder::InstallHostSpecificOpcodeHandlers() {
     {OPD(3, 0b01, 0x21), 1, &OpDispatchBuilder::VINSERTPSOp},
 
     {OPD(3, 0b01, 0x38), 1, &OpDispatchBuilder::VINSERTOp},
+    {OPD(3, 0b01, 0x39), 1, &OpDispatchBuilder::VEXTRACT128Op},
 
     {OPD(3, 0b01, 0x40), 1, &OpDispatchBuilder::VDPPOp<4>},
     {OPD(3, 0b01, 0x41), 1, &OpDispatchBuilder::VDPPOp<8>},
