45493ad3ef862151bc777f80778a80175d803fe4
Ryan Houdek <Sonicadvance1@gmail.com>
2023-03-16 01:26:51 -0700

    fextl: Add sstream

---
+++ External/FEXCore/include/FEXCore/fextl/sstream.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <FEXCore/fextl/allocator.h>
+
+#include <sstream>
+
+namespace fextl {
+  template<class CharT, class Traits = std::char_traits<CharT>, class Allocator = fextl::FEXAlloc<CharT>>
+  using basic_stringbuf = std::basic_stringbuf<CharT, Traits, Allocator>;
+
+  template<class CharT, class Traits = std::char_traits<CharT>, class Allocator = fextl::FEXAlloc<CharT>>
+  using basic_istringstream = std::basic_istringstream<CharT, Traits, Allocator>;
+
+  template<class CharT, class Traits = std::char_traits<CharT>, class Allocator = fextl::FEXAlloc<CharT>>
+  using basic_ostringstream = std::basic_ostringstream<CharT, Traits, Allocator>;
+
+  template<class CharT, class Traits = std::char_traits<CharT>, class Allocator = fextl::FEXAlloc<CharT>>
+  using basic_stringstream = std::basic_stringstream<CharT, Traits, Allocator>;
+
+  using stringbuf = fextl::basic_stringbuf<char>;
+  using istringstream = fextl::basic_istringstream<char>;
+  using ostringstream = fextl::basic_ostringstream<char>;
+  using stringstream = fextl::basic_stringstream<char>;
+}
