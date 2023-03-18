e48fb1850e5e0ec1dc5ea2e1fbce782445d7b9ab
Ryan Houdek <Sonicadvance1@gmail.com>
2023-03-17 19:47:50 -0700

    fextl: add unordered_multimap

--- External/FEXCore/include/FEXCore/fextl/unordered_map.h
+++ External/FEXCore/include/FEXCore/fextl/unordered_map.h
@@ -6,4 +6,7 @@
 namespace fextl {
   template<class Key, class T, class Hash = std::hash<Key>, class KeyEqual = std::equal_to<Key>, class Allocator = fextl::FEXAlloc<std::pair<const Key, T>>>
   using unordered_map = std::unordered_map<Key, T, Hash, KeyEqual, Allocator>;
+
+  template<class Key, class T, class Hash = std::hash<Key>, class KeyEqual = std::equal_to<Key>, class Allocator = fextl::FEXAlloc<std::pair<const Key, T>>>
+  using unordered_multimap = std::unordered_multimap<Key, T, Hash, KeyEqual, Allocator>;
 }
