a899444ddde08d2386a48f03f47722ab5d8c3249
Ryan Houdek <Sonicadvance1@gmail.com>
2023-03-23 08:11:01 -0700

    StrConv: Address review comments

--- External/FEXCore/Source/Common/StringConv.h
+++ External/FEXCore/Source/Common/StringConv.h
@@ -7,38 +7,38 @@
 
 namespace FEXCore::StrConv {
   [[maybe_unused]] static bool Conv(std::string_view Value, bool *Result) {
-    *Result = std::stoi(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stoull(Value.data(), nullptr, 0);
     return true;
   }
 
   [[maybe_unused]] static bool Conv(std::string_view Value, uint8_t *Result) {
-    *Result = std::stoi(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stoul(Value.data(), nullptr, 0);
     return true;
   }
 
   [[maybe_unused]] static bool Conv(std::string_view Value, uint16_t *Result) {
-    *Result = std::stoi(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stoul(Value.data(), nullptr, 0);
     return true;
   }
 
   [[maybe_unused]] static bool Conv(std::string_view Value, uint32_t *Result) {
-    *Result = std::stoi(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stoul(Value.data(), nullptr, 0);
     return true;
   }
 
   [[maybe_unused]] static bool Conv(std::string_view Value, int32_t *Result) {
-    *Result = std::stoi(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stol(Value.data(), nullptr, 0);
     return true;
   }
 
   [[maybe_unused]] static bool Conv(std::string_view Value, uint64_t *Result) {
-    *Result = std::stoull(fextl::string(Value).c_str(), nullptr, 0);
+    *Result = std::stoull(Value.data(), nullptr, 0);
     return true;
   }
   template <typename T,
     typename = std::enable_if<std::is_enum<T>::value, T>>
   [[maybe_unused]] static bool Conv(std::string_view Value, T *Result) {
-    *Result = static_cast<T>(std::stoull(fextl::string(Value).c_str(), nullptr, 0));
+    *Result = static_cast<T>(std::stoull(Value.data(), nullptr, 0));
     return true;
   }
 
