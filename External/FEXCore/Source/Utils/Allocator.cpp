0bb59ade53e2b7ea0506156c596701fe5cdc6282
Ryan Houdek <Sonicadvance1@gmail.com>
2023-04-10 16:21:33 -0700

    Updates jemalloc
    Needed to change some symbol names due to proper jemalloc namespacing
    now.

--- External/FEXCore/Source/Utils/Allocator.cpp
+++ External/FEXCore/Source/Utils/Allocator.cpp
@@ -29,8 +29,8 @@ extern "C" {
   typedef int (*munmap_hook_type)(void *addr, size_t length);
 
 #ifdef ENABLE_JEMALLOC
-  extern mmap_hook_type __mmap_hook;
-  extern munmap_hook_type __munmap_hook;
+  extern mmap_hook_type je___mmap_hook;
+  extern munmap_hook_type je___munmap_hook;
 #endif
 }
 
@@ -143,8 +143,8 @@ namespace FEXCore::Allocator {
   void SetupHooks() {
     Alloc64 = Alloc::OSAllocator::Create64BitAllocator();
 #ifdef ENABLE_JEMALLOC
-    __mmap_hook   = FEX_mmap;
-    __munmap_hook = FEX_munmap;
+    je___mmap_hook   = FEX_mmap;
+    je___munmap_hook = FEX_munmap;
 #endif
     FEXCore::Allocator::mmap = FEX_mmap;
     FEXCore::Allocator::munmap = FEX_munmap;
@@ -152,8 +152,8 @@ namespace FEXCore::Allocator {
 
   void ClearHooks() {
 #ifdef ENABLE_JEMALLOC
-    __mmap_hook   = ::mmap;
-    __munmap_hook = ::munmap;
+    je___mmap_hook   = ::mmap;
+    je___munmap_hook = ::munmap;
 #endif
     FEXCore::Allocator::mmap = ::mmap;
     FEXCore::Allocator::munmap = ::munmap;
