84985952c9ca745f26ae9b24c489a6a5c42647dd
Lioncache <mai.iam2048@gmail.com>
2023-04-18 07:08:45 -0400

    OpcodeDispatcher: Factor out scalar floating-point conversion to helper
    Will be used to implement the AVX variants of VCVTSD2SS and VCVTSS2SD

--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher.h
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher.h
@@ -936,6 +936,10 @@ private:
   OrderedNode* VectorRoundImpl(OpcodeArgs, size_t ElementSize,
                                OrderedNode *Src, uint64_t Mode);
 
+  OrderedNode* Scalar_CVT_Float_To_FloatImpl(OpcodeArgs, size_t DstElementSize, size_t SrcElementSize,
+                                             const X86Tables::DecodedOperand& Src1Op,
+                                             const X86Tables::DecodedOperand& Src2Op);
+
   OrderedNode* Vector_CVT_Float_To_IntImpl(OpcodeArgs, size_t SrcElementSize, bool Narrow, bool HostRoundingMode);
 
   OrderedNode* Vector_CVT_Int_To_FloatImpl(OpcodeArgs, size_t SrcElementSize, bool Widen);
--- External/FEXCore/Source/Interface/Core/OpcodeDispatcher/Vector.cpp
+++ External/FEXCore/Source/Interface/Core/OpcodeDispatcher/Vector.cpp
@@ -2126,17 +2126,20 @@ void OpDispatchBuilder::AVXVector_CVT_Float_To_Int<8, true, false>(OpcodeArgs);
 template
 void OpDispatchBuilder::AVXVector_CVT_Float_To_Int<8, true, true>(OpcodeArgs);
 
+OrderedNode* OpDispatchBuilder::Scalar_CVT_Float_To_FloatImpl(OpcodeArgs, size_t DstElementSize, size_t SrcElementSize,
+                                                              const X86Tables::DecodedOperand& Src1Op,
+                                                              const X86Tables::DecodedOperand& Src2Op) {
+  OrderedNode *Src1 = LoadSource_WithOpSize(FPRClass, Op, Src1Op, 16, Op->Flags, -1);
+  OrderedNode *Src2 = LoadSource_WithOpSize(FPRClass, Op, Src2Op, SrcElementSize, Op->Flags, -1);
+
+  OrderedNode *Converted = _Float_FToF(DstElementSize, SrcElementSize, Src2);
+
+  return _VInsElement(16, DstElementSize, 0, 0, Src1, Converted);
+}
+
 template<size_t DstElementSize, size_t SrcElementSize>
 void OpDispatchBuilder::Scalar_CVT_Float_To_Float(OpcodeArgs) {
-  const auto DstSize = GetDstSize(Op);
-
-  OrderedNode *Dest = LoadSource_WithOpSize(FPRClass, Op, Op->Dest, DstSize, Op->Flags, -1);
-  OrderedNode *Src = LoadSource(FPRClass, Op, Op->Src[0], Op->Flags, -1);
-
-  Src = _Float_FToF(DstElementSize, SrcElementSize, Src);
-  Src = _VInsElement(16, DstElementSize, 0, 0, Dest, Src);
-
-  auto Result = _VInsElement(DstSize, DstElementSize, 0, 0, Dest, Src);
+  OrderedNode *Result = Scalar_CVT_Float_To_FloatImpl(Op, DstElementSize, SrcElementSize, Op->Dest, Op->Src[0]);
   StoreResult(FPRClass, Op, Result, -1);
 }
 
