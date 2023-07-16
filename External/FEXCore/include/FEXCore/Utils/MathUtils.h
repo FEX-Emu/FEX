80ae3e632d75d1e5f0f18913929822d736a0ad65
Ryan Houdek <Sonicadvance1@gmail.com>
2023-07-16 01:17:56 -0700

    Arm64: Optimize {Load,Store}ContextIndexed address generation
    All of these IR operations were being fairly inefficient in their
    address calculation. All of these are known using power of 2 stride
    indexing. So all of these can be converted from three instructions to
    one.
    
    These are always used for x87 stack accesses so each one gets an
    improvement.
    
    Before:
    ```asm
    0x0000ffff6a800248  d2800200    mov x0, #0x10
    0x0000ffff6a80024c  9b007e80    mul x0, x20, x0
    0x0000ffff6a800250  8b000380    add x0, x28, x0
    0x0000ffff6a800254  fd417805    ldr d5, [x0, #752]
    ```
    
    After:
    ```asm
    0x0000ffff91e80240  8b141380    add x0, x28, x20, lsl #4
    0x0000ffff91e80244  fd417805    ldr d5, [x0, #752]
    ```

--- External/FEXCore/include/FEXCore/Utils/MathUtils.h
+++ External/FEXCore/include/FEXCore/Utils/MathUtils.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <FEXCore/Utils/LogManager.h>
+
+#include <bit>
 #include <cstdint>
+#include <type_traits>
 
 namespace FEXCore {
 [[nodiscard]] constexpr uint64_t AlignUp(uint64_t value, uint64_t size) {
@@ -10,4 +14,14 @@ namespace FEXCore {
 [[nodiscard]] constexpr uint64_t AlignDown(uint64_t value, uint64_t size) {
   return value - value % size;
 }
+
+// Returns the ilog2 of a power-of-2 integer.
+// Asserts in the case that the passed in integer is not a power-of-2.
+template<typename T>
+requires(std::is_unsigned_v<T>)
+[[nodiscard]] constexpr T ilog2(T Value) {
+  LOGMAN_THROW_A_FMT(std::has_single_bit(Value), "ilog2 requires popcount to be one");
+  return std::countr_zero(Value);
+}
+
 } // namespace FEXCore
