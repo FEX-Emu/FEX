ed95dfd62ef3ce30a36e0b9f1db7b5286d7b7049
Ryan Houdek <Sonicadvance1@gmail.com>
2023-08-08 22:32:17 -0700

    Config: Adds support for overriding host features
    This allows use to both enable and disable regardless of what the host
    supports. This replaces the old `EnableAVX` option.
    
    Unlike the old EnableAVX option which was a binary option which could
    only disable, each of these options are technically trinary states.
    Not setting an option gives you the default detection, while explicitly
    enabling or disabling will toggle the option regardless of what the host
    supports.
    
    This will be used by the instruction count CI in the future.

--- External/FEXCore/include/FEXCore/Utils/EnumOperators.h
+++ External/FEXCore/include/FEXCore/Utils/EnumOperators.h
@@ -7,6 +7,12 @@ static constexpr Enum operator Op(Enum lhs, Enum rhs) { \
     Type _lhs = static_cast<Type>(lhs); \
     Type _rhs = static_cast<Type>(rhs); \
     return static_cast<Enum>(_lhs Op _rhs); \
+} \
+[[maybe_unused]] \
+static constexpr uint64_t operator Op(uint64_t lhs, Enum rhs) { \
+    using Type = std::underlying_type_t<Enum>; \
+    Type _rhs = static_cast<Type>(rhs); \
+    return lhs Op _rhs; \
 }
 
 #define FEX_DEF_ENUM_CLASS_UNARY_OP(Enum, Op) \
