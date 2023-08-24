d37ce08ae9b4e197ba2946348bb885d8345b463c
Lioncache <mai.iam2048@gmail.com>
2023-08-23 21:26:01 -0400

    OpcodeDispatcher: Remove redundant move from VAESDec
    Zero-extension will occur automatically upon storing if necessary.

--- FEXCore/Source/Interface/Core/OpcodeDispatcher/Crypto.cpp
+++ FEXCore/Source/Interface/Core/OpcodeDispatcher/Crypto.cpp
@@ -331,7 +331,7 @@ void OpDispatchBuilder::AESDecOp(OpcodeArgs) {
 
 void OpDispatchBuilder::VAESDecOp(OpcodeArgs) {
   const auto DstSize = GetDstSize(Op);
-  const auto Is128Bit = DstSize == Core::CPUState::XMM_SSE_REG_SIZE;
+  [[maybe_unused]] const auto Is128Bit = DstSize == Core::CPUState::XMM_SSE_REG_SIZE;
 
   // TODO: Handle 256-bit VAESDEC.
   LOGMAN_THROW_A_FMT(Is128Bit, "256-bit VAESDEC unimplemented");
@@ -340,9 +340,6 @@ void OpDispatchBuilder::VAESDecOp(OpcodeArgs) {
   OrderedNode *Key = LoadSource(FPRClass, Op, Op->Src[1], Op->Flags, -1);
   OrderedNode *Result = _VAESDec(DstSize, State, Key);
 
-  if (Is128Bit) {
-    Result = _VMov(16, Result);
-  }
   StoreResult(FPRClass, Op, Result, -1);
 }
 
