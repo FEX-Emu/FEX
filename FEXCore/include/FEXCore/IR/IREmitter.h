e9f2bc037f2c694924e09ff74e3f04830a2455e0
Ryan Houdek <Sonicadvance1@gmail.com>
2023-08-28 05:51:40 -0700

    IR: Removes non-opsize AtomicAdd/Sub/And/Or
    These were unused

--- FEXCore/include/FEXCore/IR/IREmitter.h
+++ FEXCore/include/FEXCore/IR/IREmitter.h
@@ -115,18 +115,6 @@ friend class FEXCore::IR::PassManager;
   IRPair<IROp_CASPair> _CASPair(OrderedNode *_Expected, OrderedNode *_Desired, OrderedNode *_Addr) {
     return _CASPair(static_cast<OpSize>(GetOpSize(_Expected)), _Expected, _Desired, _Addr);
   }
-  IRPair<IROp_AtomicAdd> _AtomicAdd(uint8_t Size, OrderedNode *_Value, OrderedNode *_Addr) {
-    return _AtomicAdd(static_cast<OpSize>(Size), _Value, _Addr);
-  }
-  IRPair<IROp_AtomicSub> _AtomicSub(uint8_t Size, OrderedNode *_Value, OrderedNode *_Addr) {
-    return _AtomicSub(static_cast<OpSize>(Size), _Value, _Addr);
-  }
-  IRPair<IROp_AtomicAnd> _AtomicAnd(uint8_t Size, OrderedNode *_Value, OrderedNode *_Addr) {
-    return _AtomicAnd(static_cast<OpSize>(Size), _Value, _Addr);
-  }
-  IRPair<IROp_AtomicOr> _AtomicOr(uint8_t Size, OrderedNode *_Value, OrderedNode *_Addr) {
-    return _AtomicOr(static_cast<OpSize>(Size), _Value, _Addr);
-  }
   ///< ALU
   IRPair<IROp_Add> _Add(OrderedNode *_Src1, OrderedNode *_Src2) {
     return _Add(static_cast<OpSize>(std::max<uint8_t>(4, std::max(GetOpSize(_Src1), GetOpSize(_Src2)))), _Src1, _Src2);
