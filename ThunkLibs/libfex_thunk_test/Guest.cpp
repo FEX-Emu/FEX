bef64c53f88c287611afd98e3975002c57bab895
Tony Wasserka <neobrain@users.noreply.github.com>
2023-09-14 20:45:02 +0200

    unittests: Add test thunk library

---
+++ ThunkLibs/libfex_thunk_test/Guest.cpp
@@ -0,0 +1,12 @@
+/*
+$info$
+tags: thunklibs|fex_thunk_test
+$end_info$
+*/
+
+#include "common/Guest.h"
+#include "api.h"
+
+#include "thunkgen_guest_libfex_thunk_test.inl"
+
+LOAD_LIB(libfex_thunk_test)
