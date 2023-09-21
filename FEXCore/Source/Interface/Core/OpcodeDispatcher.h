8e9e87f631298ec0ccc34c72ce7e35672f4e621e
Ryan Houdek <Sonicadvance1@gmail.com>
2023-09-21 01:56:38 -0700

    OpcodeDispatcher: Removes non-explicit SelectCC function
    Renames the explicit sized one to `SelectCC`
    Cleans up a bit of duplicated code.

--- FEXCore/Source/Interface/Core/OpcodeDispatcher.h
+++ FEXCore/Source/Interface/Core/OpcodeDispatcher.h
@@ -1280,9 +1280,7 @@ private:
     CachedIndexedNamedVectorConstants.clear();
   }
 
-  // TODO: SelectCC is duplicated SelectCCExplicitSize. Should get removed once all users are removed.
-  OrderedNode *SelectCC(uint8_t OP, OrderedNode *TrueValue, OrderedNode *FalseValue);
-  OrderedNode *SelectCCExplicitSize(uint8_t OP, IR::OpSize ResultSize, OrderedNode *TrueValue, OrderedNode *FalseValue);
+  OrderedNode *SelectCC(uint8_t OP, IR::OpSize ResultSize, OrderedNode *TrueValue, OrderedNode *FalseValue);
 
   /**
    * @name Deferred RFLAG calculation and generation.
