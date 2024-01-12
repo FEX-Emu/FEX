cd4578de273ca5e5ef10232aab16e9e59b061f5b
Tony Wasserka <neobrain@users.noreply.github.com>
2024-01-12 14:47:10 +0100

    FEXLinuxTests/thunks: Add tests for automatic repacking

--- ThunkLibs/libfex_thunk_test/lib.cpp
+++ ThunkLibs/libfex_thunk_test/lib.cpp
@@ -30,4 +30,21 @@ uint32_t GetUnionTypeA(UnionType* value) {
   return value->a;
 }
 
+ReorderingType MakeReorderingType(uint32_t a, uint32_t b) {
+  return ReorderingType { .a = a, .b = b };
+}
+
+uint32_t GetReorderingTypeMember(ReorderingType* data, int index) {
+  if (index == 0) {
+    return data->a;
+  } else {
+    return data->b;
+  }
+}
+
+void ModifyReorderingTypeMembers(ReorderingType* data) {
+  data->a += 1;
+  data->b += 2;
+}
+
 } // extern "C"
--- ThunkLibs/libfex_thunk_test/libfex_thunk_test_interface.cpp
+++ ThunkLibs/libfex_thunk_test/libfex_thunk_test_interface.cpp
@@ -21,3 +21,10 @@ template<> struct fex_gen_config<DestroyOpaqueType> {};
 template<> struct fex_gen_type<UnionType> : fexgen::assume_compatible_data_layout {};
 template<> struct fex_gen_config<MakeUnionType> {};
 template<> struct fex_gen_config<GetUnionTypeA> {};
+
+template<> struct fex_gen_config<MakeReorderingType> {};
+template<> struct fex_gen_config<GetReorderingTypeMember> {};
+template<> struct fex_gen_config<ModifyReorderingTypeMembers> {};
+
+template<> struct fex_gen_config<QueryOffsetOf> : fexgen::custom_host_impl {};
+template<> struct fex_gen_param<QueryOffsetOf, 0, ReorderingType*> : fexgen::ptr_passthrough {};
