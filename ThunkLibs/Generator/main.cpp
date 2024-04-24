68b9bab9696f818cbdc9a47578e3e424ca952622
Tony Wasserka <neobrain@users.noreply.github.com>
2024-04-24 17:56:19 +0200

    Library Forwarding: Allow reading standard library headers from a development x86 rootfs

--- ThunkLibs/Generator/main.cpp
+++ ThunkLibs/Generator/main.cpp
@@ -4,6 +4,7 @@
 #include "llvm/Support/Signals.h"
 
 #include <iostream>
+#include <optional>
 #include <string>
 
 #include "interface.h"
@@ -17,7 +18,7 @@ void print_usage(const char* program_name) {
 int main(int argc, char* const argv[]) {
   llvm::sys::PrintStackTraceOnErrorSignal(argv[0]);
 
-  if (argc < 5) {
+  if (argc < 6) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
@@ -32,7 +33,7 @@ int main(int argc, char* const argv[]) {
   }
 
   // Process arguments before the "--" separator
-  if (argc != 5 && argc != 6) {
+  if (argc != 6 && argc != 7) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
@@ -42,6 +43,7 @@ int main(int argc, char* const argv[]) {
   const std::string libname = *arg++;
   const std::string target_abi = *arg++;
   const std::string output_filename = *arg++;
+  const std::string x86_rootfs = *arg++;
 
   OutputFilenames output_filenames;
   if (target_abi == "-host") {
@@ -65,27 +67,52 @@ int main(int argc, char* const argv[]) {
 
   ClangTool GuestTool = Tool;
 
-  {
-    const bool is_32bit_guest = (argv[5] == std::string_view {"-for-32bit-guest"});
-    auto append_guest_args = [is_32bit_guest](const clang::tooling::CommandLineArguments& Args, clang::StringRef) {
-      clang::tooling::CommandLineArguments AdjustedArgs = Args;
-      const char* platform = is_32bit_guest ? "i686" : "x86_64";
-      if (is_32bit_guest) {
-        AdjustedArgs.push_back("-m32");
-        AdjustedArgs.push_back("-DIS_32BIT_THUNK");
-      }
-      AdjustedArgs.push_back(std::string {"--target="} + platform + "-linux-unknown");
-      AdjustedArgs.push_back("-isystem");
-      AdjustedArgs.push_back(std::string {"/usr/"} + platform + "-linux-gnu/include/");
-      AdjustedArgs.push_back("-DGUEST_THUNK_LIBRARY");
-      return AdjustedArgs;
-    };
-    GuestTool.appendArgumentsAdjuster(append_guest_args);
-  }
+  auto append_x86_rootfs_includes = [&x86_rootfs](clang::tooling::CommandLineArguments& Args, const char* triple) {
+    if (x86_rootfs == "/") {
+      return;
+    }
 
+    Args.push_back("--sysroot");
+    Args.push_back(x86_rootfs);
+
+    // The dev rootfs is only really needed for the standard library.
+    // Other libraries generally don't have platform specific headers.
+    Args.push_back("-idirafter");
+    Args.push_back("/usr/include/");
+  };
+
+  // Analyse data layout for guest ABI
+  const bool is_32bit_guest = (argv[6] == std::string_view {"-for-32bit-guest"});
+  GuestTool.appendArgumentsAdjuster([&](const clang::tooling::CommandLineArguments& Args, clang::StringRef) {
+    clang::tooling::CommandLineArguments AdjustedArgs = Args;
+    const char* platform = is_32bit_guest ? "i686-linux-gnu" : "x86_64-linux-gnu";
+    if (is_32bit_guest) {
+      AdjustedArgs.push_back("-m32");
+      AdjustedArgs.push_back("-DIS_32BIT_THUNK");
+    }
+    AdjustedArgs.push_back("-DGUEST_THUNK_LIBRARY");
+    AdjustedArgs.push_back(std::string {"--target="} + platform);
+    AdjustedArgs.push_back("-isystem");
+    AdjustedArgs.push_back(std::string {"/usr/"} + platform + "/include/");
+
+    append_x86_rootfs_includes(AdjustedArgs, platform);
+
+    return AdjustedArgs;
+  });
   auto data_layout_analysis_factory = std::make_unique<AnalyzeDataLayoutActionFactory>();
   GuestTool.run(data_layout_analysis_factory.get());
   auto& data_layout = data_layout_analysis_factory->GetDataLayout();
 
+  // Run generator for target ABI
+  Tool.appendArgumentsAdjuster([&](const clang::tooling::CommandLineArguments& Args, clang::StringRef) {
+    clang::tooling::CommandLineArguments AdjustedArgs = Args;
+    AdjustedArgs.push_back("-DIS_HOST_THUNKGEN_PASS");
+    if (target_abi == "-guest") {
+      const char* platform = is_32bit_guest ? "i686-linux-gnu" : "x86_64-linux-gnu";
+      append_x86_rootfs_includes(AdjustedArgs, platform);
+    }
+
+    return AdjustedArgs;
+  });
   return Tool.run(std::make_unique<GenerateThunkLibsActionFactory>(std::move(libname), std::move(output_filenames), data_layout).get());
 }
