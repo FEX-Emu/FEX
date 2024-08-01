80c9a43eefad9857cfd1cf034939573989e672b0
Billy Laws <blaws05@gmail.com>
2024-08-01 11:30:23 +0000

    Windows: Fallback to a log file for debug output on Windows
    OutputDebugString etc are exception based and thus don't really work for
    FEX's needs as often times logs can happen in places where exceptions
    cannot be thrown.

--- Source/Windows/Common/Logging.cpp
+++ Source/Windows/Common/Logging.cpp
@@ -2,6 +2,7 @@
 #include <FEXCore/fextl/fmt.h>
 #include <FEXCore/Utils/LogManager.h>
 
+#include <cstdio>
 #include <ntstatus.h>
 #include <windef.h>
 #include <winternl.h>
@@ -9,21 +10,34 @@
 
 namespace {
 void (*WineDbgOut)(const char* Message);
+FILE* LogFile;
 
-void MsgHandler(LogMan::DebugLevels Level, const char* Message) {
+static void MsgHandler(LogMan::DebugLevels Level, const char* Message) {
   const auto Output = fextl::fmt::format("[{}][{:X}] {}\n", LogMan::DebugLevelStr(Level), GetCurrentThreadId(), Message);
-  WineDbgOut(Output.c_str());
+  if (WineDbgOut) {
+    WineDbgOut(Output.c_str());
+  } else {
+    fwrite(Output.c_str(), 1, Output.size(), LogFile);
+  }
 }
 
-void AssertHandler(const char* Message) {
+static void AssertHandler(const char* Message) {
   const auto Output = fextl::fmt::format("[ASSERT] {}\n", Message);
-  WineDbgOut(Output.c_str());
+  if (WineDbgOut) {
+    WineDbgOut(Output.c_str());
+  } else {
+    fwrite(Output.c_str(), 1, Output.size(), LogFile);
+  }
 }
 } // namespace
 
 namespace FEX::Windows::Logging {
 void Init() {
   WineDbgOut = reinterpret_cast<decltype(WineDbgOut)>(GetProcAddress(GetModuleHandleA("ntdll.dll"), "__wine_dbg_output"));
+  if (!WineDbgOut) {
+    const auto Path = fextl::fmt::format("{}\\fex-{}.log", getenv("LOCALAPPDATA"), GetCurrentProcessId());
+    LogFile = fopen(Path.c_str(), "a");
+  }
   LogMan::Throw::InstallHandler(AssertHandler);
   LogMan::Msg::InstallHandler(MsgHandler);
 }
