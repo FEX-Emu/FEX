b87092aecf6a31b7704217972e0def6386e2d011
Billy Laws <blaws05@gmail.com>
2024-08-06 16:02:44 +0000

    HostFeatures: Split out vixl feature and CTR/MIDR detection

--- Source/Common/HostFeatures.h
+++ Source/Common/HostFeatures.h
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 #include <FEXCore/Core/HostFeatures.h>
+#include "aarch64/cpu-aarch64.h"
 
 namespace FEX {
+FEXCore::HostFeatures FetchHostFeatures(vixl::CPUFeatures Features, uint64_t CTR, uint64_t MIDR);
 FEXCore::HostFeatures FetchHostFeatures();
 }
