129ec63e92af23b81bf7d192d215fc012770627f
Ryan Houdek <Sonicadvance1@gmail.com>
2024-08-25 02:19:28 -0700

    x64/Time: Add EFAULT checks

--- Source/Tools/LinuxEmulation/LinuxSyscalls/x64/Time.cpp
+++ Source/Tools/LinuxEmulation/LinuxSyscalls/x64/Time.cpp
@@ -24,16 +24,21 @@ void RegisterTime(FEX::HLE::SyscallHandler* Handler) {
   using namespace FEXCore::IR;
   REGISTER_SYSCALL_IMPL_X64_FLAGS(time, SyscallFlags::OPTIMIZETHROUGH | SyscallFlags::NOSYNCSTATEONENTRY,
                                   [](FEXCore::Core::CpuStateFrame* Frame, time_t* tloc) -> uint64_t {
+                                    FaultSafeUserMemAccess::VerifyIsWritableOrNull(tloc, sizeof(time_t));
                                     uint64_t Result = ::time(tloc);
                                     SYSCALL_ERRNO();
                                   });
 
-  REGISTER_SYSCALL_IMPL_X64(utime, [](FEXCore::Core::CpuStateFrame* Frame, char* filename, const struct utimbuf* times) -> uint64_t {
+  REGISTER_SYSCALL_IMPL_X64(utime, [](FEXCore::Core::CpuStateFrame* Frame, const char* filename, const struct utimbuf* times) -> uint64_t {
+    FaultSafeUserMemAccess::VerifyIsStringReadable(filename);
+    FaultSafeUserMemAccess::VerifyIsReadableOrNull(times, sizeof(utimbuf));
     uint64_t Result = ::utime(filename, times);
     SYSCALL_ERRNO();
   });
 
   REGISTER_SYSCALL_IMPL_X64(utimes, [](FEXCore::Core::CpuStateFrame* Frame, const char* filename, const struct timeval times[2]) -> uint64_t {
+    FaultSafeUserMemAccess::VerifyIsStringReadable(filename);
+    FaultSafeUserMemAccess::VerifyIsReadableOrNull(times, sizeof(timeval) * 2);
     uint64_t Result = ::utimes(filename, times);
     SYSCALL_ERRNO();
   });
