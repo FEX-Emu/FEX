ab561d34a8086556f5a437d9251cc6c577ba1dc9
Ryan Houdek <Sonicadvance1@gmail.com>
2024-09-15 19:47:12 -0700

    FEXServer: Move over to MicroArgParser

--- Source/Tools/FEXServer/ArgumentLoader.cpp
+++ Source/Tools/FEXServer/ArgumentLoader.cpp
@@ -1,37 +1,36 @@
 // SPDX-License-Identifier: MIT
 #include "ArgumentLoader.h"
-#include "Common/cpp-optparse/OptionParser.h"
+#include "Common/MicroArgParser.h"
+#include <FEXCore/fextl/fmt.h>
 
 #include "git_version.h"
 
-#include <fmt/format.h>
-
 namespace FEXServer::Config {
-static fextl::string Version = "FEX-Emu (" GIT_DESCRIBE_STRING ") ";
 
 FEXServerOptions Load(int argc, char** argv) {
+  static std::array<FEX::MicroArgParser::ParseMember, 6> Args = {{
+    {FEX::MicroArgParser::ParseMember::Type::Bool, "-k", "--kill", "Shutdown an already active FEXServer", "0"},
+    {FEX::MicroArgParser::ParseMember::Type::Bool, "-f", "--foreground", "Run this FEXServer in the foreground", "0"},
+    {FEX::MicroArgParser::ParseMember::Type::IntOptional, "-p", "--persistent", "Make FEXServer persistent. Optional number of seconds.", "0"},
+    {FEX::MicroArgParser::ParseMember::Type::Bool, "-w", "--wait", "Wait for the FEXServer to shutdown", "0"},
+    // Defaults
+    {FEX::MicroArgParser::ParseMember::Type::Bool, "-v", "--version", "show program's verison number and exit", "0"},
+    {FEX::MicroArgParser::ParseMember::Type::Bool, "-h", "--help", "show this help message and exit", "0"},
+  }};
+
+  FEX::MicroArgParser Parser(Args);
+  Parser.Version("FEX-Emu (" GIT_DESCRIBE_STRING ") ");
+  Parser.Parse(argc, argv);
+
   FEXServerOptions FEXOptions {};
-  optparse::OptionParser Parser = optparse::OptionParser().version(Version);
-
-  Parser.add_option("-k", "--kill").action("store_true").set_default(false).help("Shutdown an already active FEXServer");
-
-  Parser.add_option("-f", "--foreground").action("store_true").set_default(false).help("Run this FEXServer in the foreground");
-
-  Parser.add_option("-p", "--persistent").action("store").type("int").set_default(0).set_optional_value(true).metavar("n").help("Make FEXServer persistent. Optional number of seconds");
-
-  Parser.add_option("-w", "--wait").action("store_true").set_default(false).help("Wait for the FEXServer to shutdown");
-
-  Parser.add_option("-v").action("version").help("Version string");
-
-  optparse::Values Options = Parser.parse_args(argc, argv);
-
-  FEXOptions.Kill = Options.get("kill");
-  FEXOptions.Foreground = Options.get("foreground");
-  FEXOptions.Wait = Options.get("wait");
+  FEXOptions.Kill = Parser.Get<bool>("-k");
+  FEXOptions.Foreground = Parser.Get<bool>("-f");
+  FEXOptions.Wait = Parser.Get<bool>("-w");
   if (FEXOptions.Wait) {
     FEXOptions.Foreground = true;
   }
-  FEXOptions.PersistentTimeout = Options.get("persistent");
+
+  FEXOptions.PersistentTimeout = Parser.Get<uint32_t>("-p");
 
   return FEXOptions;
 }
