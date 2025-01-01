5d47b9195b8b31561774660255d279536aabb26e
Ryan Houdek <Sonicadvance1@gmail.com>
2024-12-31 21:59:12 -0800

    unittests/ASM: Adds missing MMX PADDQ test

---
+++ unittests/ASM/TwoByte/0F_D4.asm
@@ -0,0 +1,29 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "MM0": "0xa2a4a6a8aaacaeb0",
+    "MM1": "0xa2a4a6a8aaacaeb0"
+  }
+}
+%endif
+
+mov rdx, 0xe0000000
+
+mov rax, 0x6162636465666768
+mov [rdx + 8 * 0], rax
+mov rax, 0x7172737475767778
+mov [rdx + 8 * 1], rax
+
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 2], rax
+mov rax, 0x5152535455565758
+mov [rdx + 8 * 3], rax
+
+movq mm0, [rdx]
+paddq mm0, [rdx + 8 * 2]
+
+movq mm1, [rdx]
+movq mm2, [rdx + 8 * 2]
+paddq mm1, mm2
+
+hlt
