8efa5febd098bcce7ebae5dc17af94a0ff1496dc
Ryan Houdek <Sonicadvance1@gmail.com>
2025-01-03 09:02:04 -0800

    LinuxSyscalls: Log unhandled clone3 fork flags
    Make sure to pass the clone3 arguments all the way to the fork handler
    so it can check the flags. Currently nothing I know of uses fork plus
    the new clone3 flags, but it would be hard to see without any logging.

--- Source/Tools/LinuxEmulation/LinuxSyscalls/Syscalls/Thread.h
+++ Source/Tools/LinuxEmulation/LinuxSyscalls/Syscalls/Thread.h
@@ -20,6 +20,5 @@ struct ThreadStateObject;
 FEX::HLE::ThreadStateObject* CreateNewThread(FEXCore::Context::Context* CTX, FEXCore::Core::CpuStateFrame* Frame, FEX::HLE::clone3_args* args);
 uint64_t HandleNewClone(FEX::HLE::ThreadStateObject* Thread, FEXCore::Context::Context* CTX, FEXCore::Core::CpuStateFrame* Frame,
                         FEX::HLE::clone3_args* GuestArgs);
-uint64_t ForkGuest(FEXCore::Core::InternalThreadState* Thread, FEXCore::Core::CpuStateFrame* Frame, uint32_t flags, void* stack,
-                   size_t StackSize, pid_t* parent_tid, pid_t* child_tid, void* tls, uint64_t exit_signal);
+uint64_t ForkGuest(FEXCore::Core::InternalThreadState* Thread, FEXCore::Core::CpuStateFrame* Frame, FEX::HLE::clone3_args* args);
 } // namespace FEX::HLE
