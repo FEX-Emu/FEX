602c5306156e4472acd57d7ba3116e407fd480c0
Ryan Houdek <Sonicadvance1@gmail.com>
2025-01-22 18:41:13 -0800

    Wine: Add support for magic fex+wine shm path
    Fallback to the previous path if it doesn't exist.

--- Source/Windows/Common/Profiler.cpp
+++ Source/Windows/Common/Profiler.cpp
@@ -21,11 +21,44 @@ __attribute__((naked)) uint64_t linux_getpid() {
 }
 
 uint32_t StatAlloc::FrontendAllocateSlots(uint32_t NewSize) {
-  LogMan::Msg::DFmt("Ran out of slots. Can't allocate more");
+  if (CurrentSize == MAX_STATS_SIZE || !UsingNTQueryPath) {
+    LogMan::Msg::DFmt("Ran out of slots. Can't allocate more");
+    return CurrentSize;
+  }
+
+  MEMORY_FEX_STATS_SHM_INFORMATION Info {
+    .shm_base = nullptr,
+    .map_size = std::min(CurrentSize * 2, MAX_STATS_SIZE),
+    .max_size = MAX_STATS_SIZE,
+  };
+  size_t Length {};
+  auto Result = NtQueryVirtualMemory(NtCurrentProcess(), nullptr, MemoryFexStatsShm, &Info, sizeof(Info), &Length);
+  if (!Result) {
+    CurrentSize = Info.map_size;
+  }
+
   return CurrentSize;
 }
 
 StatAlloc::StatAlloc(FEXCore::Profiler::AppType AppType) {
+  // Try wine+fex magic path.
+
+  {
+    MEMORY_FEX_STATS_SHM_INFORMATION Info {
+      .shm_base = nullptr,
+      .map_size = 4096,
+      .max_size = MAX_STATS_SIZE,
+    };
+    size_t Length {};
+    auto Result = NtQueryVirtualMemory(NtCurrentProcess(), nullptr, MemoryFexStatsShm, &Info, sizeof(Info), &Length);
+    if (!Result) {
+      UsingNTQueryPath = true;
+      CurrentSize = Info.map_size;
+      Base = Info.shm_base;
+      SaveHeader(AppType);
+      return;
+    }
+  }
   CurrentSize = MAX_STATS_SIZE;
 
   auto handle = CreateFile(fextl::fmt::format("/dev/shm/fex-{}-stats", linux_getpid()).c_str(), GENERIC_READ | GENERIC_WRITE,
