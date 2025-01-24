177e9c559c175f1735f1f69c53ddfdc798409a01
Tony Wasserka <neobrain@users.noreply.github.com>
2025-01-24 17:21:36 +0100

    Profiler: Fix 32-bit applications

--- Source/Tools/FEXLoader/FEXLoader.cpp
+++ Source/Tools/FEXLoader/FEXLoader.cpp
@@ -414,7 +414,6 @@ int main(int argc, char** argv, char** const envp) {
     std::this_thread::sleep_for(std::chrono::seconds(StartupSleep()));
   }
 
-  FEXCore::Profiler::Init(Program.ProgramName, Program.ProgramPath);
   FEXCore::Telemetry::Initialize();
 
   if (!LDPath().empty() && Program.ProgramPath.starts_with(LDPath())) {
@@ -517,6 +516,8 @@ int main(int argc, char** argv, char** const envp) {
     } while (reinterpret_cast<uintptr_t>(data) >> 32 != 0);
     free(data);
   }
+
+  FEXCore::Profiler::Init(Program.ProgramName, Program.ProgramPath);
 
   // System allocator is now system allocator or FEX
   FEXCore::Context::InitializeStaticTables(Loader.Is64BitMode() ? FEXCore::Context::MODE_64BIT : FEXCore::Context::MODE_32BIT);
