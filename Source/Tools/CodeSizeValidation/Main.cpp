f6b4c76d76235f09d21ce0ee156c90c60a53884a
Ryan Houdek <Sonicadvance1@gmail.com>
2025-06-02 12:10:39 -0700

    InstcountCI: Adds tests for instructions discovered by #4597
    Apparently I completely missed that cpuid, xgetbv, syscall,
    l{u,}{div,rem} were failing to hit their optimized cases for inlining
    and avoiding 128-bit software divide.
    
    The divisions are a clear performance regression for 64-bit applications
    since that is the only real way to do a 64-bit division on x86, I added
    those specifically because it sped up games.
    
    CPUID depends heavily on the game, since some games use that as a
    serialization instruction fairly heavily.
    
    XGETBV is trivial since it matches behaviour of CPUID (and is basically
    an extension of it).
    
    Syscall inlining can save a decent amount of time, again heavily depends
    on game.
    
    Adds multi-inst tests for all of these situations so that once it gets
    fixed (Apparently broken once RCLSE got stripped out), we can see that
    they keep working. Obviously tests couldn't have existed in instcountCI
    before since we didn't support multi-instruction tests.

--- Source/Tools/CodeSizeValidation/Main.cpp
+++ Source/Tools/CodeSizeValidation/Main.cpp
@@ -434,6 +434,35 @@ public:
 private:
   fextl::vector<std::pair<std::string_view, std::string_view>> Env;
 };
+
+class SimpleSyscallHandler : public FEXCore::HLE::SyscallHandler, public FEXCore::Allocator::FEXAllocOperators {
+public:
+  SimpleSyscallHandler() {
+    // Just claim to be linux 64-bit for simplicity.
+    OSABI = FEXCore::HLE::SyscallOSABI::OS_LINUX64;
+  }
+  uint64_t HandleSyscall(FEXCore::Core::CpuStateFrame* Frame, FEXCore::HLE::SyscallArguments* Args) override {
+    // Don't do anything
+    return 0;
+  }
+
+  FEXCore::HLE::SyscallABI GetSyscallABI(uint64_t Syscall) override {
+    if (Syscall == 0) {
+      // Claim syscall 0 is simple for instcountci inline tests.
+      return FEXCore::HLE::SyscallABI {
+        .NumArgs = 0,
+        .HasReturn = true,
+        .HostSyscallNumber = 0, // Just map to host syscall zero, it isn't going to get called.
+      };
+    }
+    return {0, false, -1};
+  }
+
+  // These are no-ops implementations of the SyscallHandler API
+  FEXCore::HLE::AOTIRCacheEntryLookupResult LookupAOTIRCacheEntry(FEXCore::Core::InternalThreadState* Thread, uint64_t GuestAddr) override {
+    return {0, 0};
+  }
+};
 } // namespace
 
 int main(int argc, char** argv, char** const envp) {
@@ -620,7 +649,7 @@ int main(int argc, char** argv, char** const envp) {
   }
 
   auto SignalDelegation = FEX::DummyHandlers::CreateSignalDelegator();
-  auto SyscallHandler = FEX::DummyHandlers::CreateSyscallHandler();
+  auto SyscallHandler = fextl::make_unique<SimpleSyscallHandler>();
 
   CTX->SetSignalDelegator(SignalDelegation.get());
   CTX->SetSyscallHandler(SyscallHandler.get());
