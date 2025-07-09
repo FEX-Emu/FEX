d01a48b773b33ebea6c50d31721cec3313e1cc3b
Ryan Houdek <Sonicadvance1@gmail.com>
2025-07-09 08:50:19 -0700

    unittests: Update vcvtsd2si test
    This would have failed prior to #4647 getting merged.

--- unittests/ASM/VEX/vcvtsd2si.asm
+++ unittests/ASM/VEX/vcvtsd2si.asm
@@ -5,7 +5,11 @@
     "RAX": "0x0000000000000001",
     "RBX": "0x0000000000000002",
     "RCX": "0x0000000000000003",
-    "RDX": "0x0000000000000004"
+    "RDX": "0x0000000000000004",
+    "RSI": "0x00000000ffffffff",
+    "RDI": "0xfffffffffffffffe",
+    "RSP": "0x00000000fffffffd",
+    "RBP": "0xfffffffffffffffc"
   }
 }
 %endif
@@ -21,6 +25,11 @@ vcvtsd2si rbx, xmm1
 vcvtsd2si ecx, [rdx + 8 * 4]
 vcvtsd2si rdx, [rdx + 8 * 6]
 
+vcvtsd2si esi, [rel .data + 8 * 8]
+vcvtsd2si rdi, [rel .data + 8 * 10]
+vcvtsd2si esp, [rel .data + 8 * 12]
+vcvtsd2si rbp, [rel .data + 8 * 14]
+
 hlt
 
 align 32
@@ -35,4 +44,16 @@ dq 0x4008000000000000
 dq 0x5152535455565758
 
 dq 0x4010000000000000
+dq 0x5152535455565758
+
+dq 0xBFF0000000000000
+dq 0x5152535455565758
+
+dq 0xC000000000000000
+dq 0x5152535455565758
+
+dq 0xC008000000000000
+dq 0x5152535455565758
+
+dq 0xC010000000000000
 dq 0x5152535455565758
