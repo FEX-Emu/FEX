b486aeeac5a70f182e04282650d02c2c63737866
Ryan Houdek <Sonicadvance1@gmail.com>
2025-08-13 16:20:18 -0700

    unittests/ASM: Unittests for callf/retf
    Not too crazy, just make sure to check RSP locations on both sides.

---
+++ unittests/ASM/PrimaryGroup/5_FF_05_03_o64_imm.asm
@@ -0,0 +1,27 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "RAX": "1",
+    "RBX": "0xe0000fe0",
+    "RSP": "0xe00090f0"
+  }
+}
+%endif
+
+mov rsp, 0xe000_1000
+mov ax, cs
+lea edi, [rel .success]
+
+sub rsp, 16
+mov [rsp], edi
+mov [rsp+4], cs
+
+mov rax, 0
+o64 call far [rsp]
+
+hlt
+
+.success:
+mov rax, 1
+mov rbx, rsp
+o64 retf 0x8100
