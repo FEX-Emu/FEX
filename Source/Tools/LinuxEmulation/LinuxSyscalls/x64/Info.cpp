8a26d3615dd2800b8fd300a0edd6892afb64d74a
Ryan Houdek <Sonicadvance1@gmail.com>
2025-09-22 15:33:48 -0700

    x64/Info: Remove lambda

--- Source/Tools/LinuxEmulation/LinuxSyscalls/x64/Info.cpp
+++ Source/Tools/LinuxEmulation/LinuxSyscalls/x64/Info.cpp
@@ -15,16 +15,17 @@ $end_info$
 #include <sys/sysinfo.h>
 
 namespace FEX::HLE::x64 {
+auto map_shadow_stack(FEXCore::Core::CpuStateFrame* Frame, uint64_t addr, uint64_t size, uint32_t flags) -> uint64_t {
+  if (FEX::HLE::_SyscallHandler->IsHostKernelVersionAtLeast(6, 6, 0)) {
+    // Claim that shadow stack isn't supported.
+    return -EOPNOTSUPP;
+  } else {
+    return -ENOSYS;
+  }
+}
+
 void RegisterInfo(FEX::HLE::SyscallHandler* Handler) {
   using namespace FEXCore::IR;
-
-  if (Handler->IsHostKernelVersionAtLeast(6, 6, 0)) {
-    REGISTER_SYSCALL_IMPL_X64(map_shadow_stack, [](FEXCore::Core::CpuStateFrame* Frame, uint64_t addr, uint64_t size, uint32_t flags) -> uint64_t {
-      // Claim that shadow stack isn't supported.
-      return -EOPNOTSUPP;
-    });
-  } else {
-    REGISTER_SYSCALL_IMPL_X64(map_shadow_stack, UnimplementedSyscallSafe);
-  }
+  REGISTER_SYSCALL_IMPL_X64(map_shadow_stack, map_shadow_stack);
 }
 } // namespace FEX::HLE::x64
