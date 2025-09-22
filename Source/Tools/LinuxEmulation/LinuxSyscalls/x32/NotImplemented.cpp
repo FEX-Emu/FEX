15d6cb32dfb947a0063f051d7aa07e33ddb5d729
Ryan Houdek <Sonicadvance1@gmail.com>
2025-09-22 13:07:07 -0700

    x32/NotImplemented: Remove lambda

--- Source/Tools/LinuxEmulation/LinuxSyscalls/x32/NotImplemented.cpp
+++ Source/Tools/LinuxEmulation/LinuxSyscalls/x32/NotImplemented.cpp
@@ -24,20 +24,43 @@ namespace FEX::HLE::x32 {
 #define REGISTER_SYSCALL_NO_PERM_X32(name) \
   REGISTER_SYSCALL_IMPL_X32(name, [](FEXCore::Core::CpuStateFrame* Frame) -> uint64_t { return -EPERM; });
 
+#define SYSCALL_NOT_IMPL_X32(name)                                 \
+  auto name(FEXCore::Core::CpuStateFrame* Frame) -> uint64_t {     \
+    LogMan::Msg::DFmt("Using deprecated/removed syscall: " #name); \
+    return -ENOSYS;                                                \
+  };
+#define SYSCALL_NO_PERM_X32(name)                              \
+  auto name(FEXCore::Core::CpuStateFrame* Frame) -> uint64_t { \
+    return -EPERM;                                             \
+  };
+
+SYSCALL_NOT_IMPL_X32(_break);
+SYSCALL_NOT_IMPL_X32(stty);
+SYSCALL_NOT_IMPL_X32(gtty);
+SYSCALL_NOT_IMPL_X32(prof);
+SYSCALL_NOT_IMPL_X32(ftime);
+SYSCALL_NOT_IMPL_X32(mpx);
+SYSCALL_NOT_IMPL_X32(lock);
+SYSCALL_NOT_IMPL_X32(ulimit);
+SYSCALL_NOT_IMPL_X32(profil);
+SYSCALL_NOT_IMPL_X32(idle);
+
+SYSCALL_NO_PERM_X32(stime);
+SYSCALL_NO_PERM_X32(bdflush);
 // these are removed/not implemented in the linux kernel we present
 void RegisterNotImplemented(FEX::HLE::SyscallHandler* Handler) {
-  REGISTER_SYSCALL_NOT_IMPL_X32(break);
-  REGISTER_SYSCALL_NOT_IMPL_X32(stty);
-  REGISTER_SYSCALL_NOT_IMPL_X32(gtty);
-  REGISTER_SYSCALL_NOT_IMPL_X32(prof);
-  REGISTER_SYSCALL_NOT_IMPL_X32(ftime);
-  REGISTER_SYSCALL_NOT_IMPL_X32(mpx);
-  REGISTER_SYSCALL_NOT_IMPL_X32(lock);
-  REGISTER_SYSCALL_NOT_IMPL_X32(ulimit);
-  REGISTER_SYSCALL_NOT_IMPL_X32(profil);
-  REGISTER_SYSCALL_NOT_IMPL_X32(idle);
+  REGISTER_SYSCALL_IMPL_X32(break, _break);
+  REGISTER_SYSCALL_IMPL_X32(stty, stty);
+  REGISTER_SYSCALL_IMPL_X32(gtty, gtty);
+  REGISTER_SYSCALL_IMPL_X32(prof, prof);
+  REGISTER_SYSCALL_IMPL_X32(ftime, ftime);
+  REGISTER_SYSCALL_IMPL_X32(mpx, mpx);
+  REGISTER_SYSCALL_IMPL_X32(lock, lock);
+  REGISTER_SYSCALL_IMPL_X32(ulimit, ulimit);
+  REGISTER_SYSCALL_IMPL_X32(profil, profil);
+  REGISTER_SYSCALL_IMPL_X32(idle, idle);
 
-  REGISTER_SYSCALL_NO_PERM_X32(stime);
-  REGISTER_SYSCALL_NO_PERM_X32(bdflush);
+  REGISTER_SYSCALL_IMPL_X32(stime, stime);
+  REGISTER_SYSCALL_IMPL_X32(bdflush, bdflush);
 }
 } // namespace FEX::HLE::x32
