9900e21e7cb4179fff63f84c304b3ba2c0e357d8
Tony Wasserka <neobrain@users.noreply.github.com>
2025-09-25 20:34:44 +0200

    Config: Add interface to query cache directory

--- Source/Common/Config.cpp
+++ Source/Common/Config.cpp
@@ -602,6 +602,12 @@ fextl::string GetConfigDirectory(bool Global, const PortableInformation& Portabl
   return ConfigDir;
 }
 
+fextl::string GetCacheDirectory() {
+  const char* HomeDir = GetHomeDirectory();
+  const char* CacheXDG = getenv("XDG_CACHE_HOME");
+  return (CacheXDG ? fextl::string {CacheXDG} : (fextl::string {HomeDir} + "/.cache")) + "/fex-emu/";
+}
+
 fextl::string GetConfigFileLocation(bool Global, const PortableInformation& PortableInfo) {
   return GetConfigDirectory(Global, PortableInfo) + "Config.json";
 }
