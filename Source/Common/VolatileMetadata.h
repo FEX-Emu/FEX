1758648f563ff129cf648de7eb6d7fcf2f3e6395
Ryan Houdek <Sonicadvance1@gmail.com>
2025-09-29 14:31:16 -0700

    Common: Move `ApplyFEXExtendedVolatileMetadata` to Common
    It is going to be used by Linux.
    
    Also adds support for file offset handling, which is required on Linux
    but not on win32.

--- Source/Common/VolatileMetadata.h
+++ Source/Common/VolatileMetadata.h
@@ -16,4 +16,36 @@ struct ExtendedVolatileMetadata {
 };
 
 fextl::unordered_map<fextl::string, ExtendedVolatileMetadata> ParseExtendedVolatileMetadata(std::string_view ListOfDescriptors);
+
+inline void ApplyFEXExtendedVolatileMetadata(FEX::VolatileMetadata::ExtendedVolatileMetadata& ExtendedMetaData,
+                                             fextl::set<uint64_t>& VolatileInstructions, FEXCore::IntervalList<uint64_t>& VolatileValidRanges,
+                                             uint64_t Address, uint64_t EndAddress, uint64_t FileOffset = 0, uint64_t FileOffsetEnd = ~0ULL) {
+  // Load FEX extended volatile metadata.
+  // Walk the volatile instructions first if they exist.
+  for (const auto it_inst : ExtendedMetaData.VolatileInstructions) {
+    const auto inst_address = it_inst + Address;
+    if (inst_address < EndAddress) {
+      VolatileInstructions.emplace(Address + it_inst);
+    } else {
+      LogMan::Msg::DFmt("Volatile instruction 0x{:x} couldn't fit in to module range [0x{:x}, 0x{:x}). Not adding anymore volatile "
+                        "instructions. Inspect your config!",
+                        inst_address, Address, EndAddress);
+      return;
+    }
+  }
+
+  // Walk the volatile list
+  for (const auto it_ranges : ExtendedMetaData.VolatileValidRanges) {
+    if (it_ranges.Offset >= FileOffset && it_ranges.End < FileOffsetEnd) {
+      VolatileValidRanges.Insert({Address + it_ranges.Offset - FileOffset, Address + it_ranges.End - FileOffset});
+    }
+  }
+
+  // If it is fully disabled, then set the entire module range
+  if (ExtendedMetaData.ModuleTSODisabled) {
+    VolatileValidRanges.Clear();
+    VolatileValidRanges.Insert({Address, EndAddress});
+  }
+}
+
 } // namespace FEX::VolatileMetadata
--- Source/Windows/ARM64EC/Module.cpp
+++ Source/Windows/ARM64EC/Module.cpp
@@ -39,10 +39,10 @@ $end_info$
 #include "Common/CRT/CRT.h"
 #include "Common/PortabilityInfo.h"
 #include "Common/Handle.h"
+#include "Common/VolatileMetadata.h"
 #include "DummyHandlers.h"
 #include "BTInterface.h"
 #include "Windows/Common/SHMStats.h"
-#include "Windows/Common/VolatileMetadata.h"
 
 #include <cstdint>
 #include <cstdio>
@@ -314,7 +314,7 @@ void LoadImageVolatileMetadata(const fextl::string& ModuleName, uint64_t Address
 
   auto it = ExtendedMetaData.find(ModuleName);
   if (it != ExtendedMetaData.end()) {
-    FEX::Windows::ApplyFEXExtendedVolatileMetadata(it->second, VolatileInstructions, VolatileValidRanges, Address, EndAddress);
+    FEX::VolatileMetadata::ApplyFEXExtendedVolatileMetadata(it->second, VolatileInstructions, VolatileValidRanges, Address, EndAddress);
   }
 
   if (VolatileInstructions.empty() && VolatileValidRanges.Empty()) {
