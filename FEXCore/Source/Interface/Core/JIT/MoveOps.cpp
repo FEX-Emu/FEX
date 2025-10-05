4f6800b768a6ed605bd9e9bb9f3d87a5c2da16a5
Lioncache <mai.iam2048@gmail.com>
2025-10-05 13:26:49 -0400

    IR: Remove Swap1/Swap2 ops
    These are no longer used.

--- FEXCore/Source/Interface/Core/JIT/MoveOps.cpp
+++ FEXCore/Source/Interface/Core/JIT/MoveOps.cpp
@@ -18,18 +18,4 @@ DEF_OP(RMWHandle) {
   mov(ARMEmitter::Size::i64Bit, GetReg(Node), GetReg(IROp->Args[0]));
 }
 
-DEF_OP(Swap1) {
-  auto Op = IROp->C<IR::IROp_Swap1>();
-  auto A = GetReg(Op->A), B = GetReg(Op->B);
-  LOGMAN_THROW_A_FMT(B == GetReg(Node), "Invariant");
-
-  mov(ARMEmitter::Size::i64Bit, TMP1, A);
-  mov(ARMEmitter::Size::i64Bit, A, B);
-  mov(ARMEmitter::Size::i64Bit, B, TMP1);
-}
-
-DEF_OP(Swap2) {
-  // Implemented above
-}
-
 } // namespace FEXCore::CPU
