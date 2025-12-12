55dbab2b3b38068636712c2de7315bfddb897253
badumbatish <tanghocle456@gmail.com>
2025-12-12 21:01:27 +0000

    Initial implementation for regex engine

---
+++ FEXCore/include/FEXCore/Utils/Regex.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "FEXCore/fextl/memory.h"
+#include "FEXCore/fextl/string.h"
+#include <FEXCore/fextl/map.h>
+#include <FEXCore/fextl/stack.h>
+#include <FEXCore/fextl/set.h>
+#include <FEXCore/fextl/vector.h>
+
+namespace FEXCore::Utils {
+
+class State {
+public:
+  fextl::vector<State *> epsilonTransitions;
+  fextl::map<char, fextl::vector<State *>> transitions;
+  bool isAccepting;
+  State(bool accepting = false) : isAccepting(accepting) {}
+  void addEpsilonTransition(State *nextState);
+  void addTransition(char c, State *nextState);
+};
+class NFA {
+  static inline fextl::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+public:
+  State *startState;
+  State *acceptingState;
+  fextl::vector<fextl::unique_ptr<State>> states;
+
+  NFA();
+  // Transfers the ownership of the states (unique_ptr) of other NFA to the
+  // current NFA.
+  void acquireStatesFrom(NFA &other);
+
+  // Functions for creating NFA using the McNaughton-Yamada-Thompson algorithm
+  static NFA createForEpsilon();
+  static NFA createForChar(char c);
+  static NFA createForDot();
+  static NFA createForUnion(NFA &nfa1, NFA &nfa2);
+  static NFA createForConcatenation(NFA &nfa1, NFA &nfa2);
+  static NFA createForKleeneStar(NFA &originalNFA);
+  static NFA createForPlus(NFA &originalNFA);
+  static fextl::set<State *> epsilonClosure(const fextl::set<State *> &states);
+  static fextl::set<State *> move(const fextl::set<State *> &states, char c);
+};
+
+// TODO: probably an NFA vector would be better instead of State vector inside
+// each NFA
+class Regex {
+  fextl::string pattern;
+  int pos;
+  NFA nfa;
+
+  // Top level parser, calls parseUnion
+  NFA parseExpression();
+
+  // INFO: "a|b"
+  NFA parseUnion();
+
+  // INFO: "ab"
+  NFA parseConcatenation();
+
+  // INFO: "a*", ".*"
+  NFA parseStar();
+
+  // INFO: "(abc)" or a
+  NFA parseAtom();
+
+public:
+  Regex(const fextl::string &s);
+  bool matches(const fextl::string &s);
+};
+} // namespace FEXCore::Utils
