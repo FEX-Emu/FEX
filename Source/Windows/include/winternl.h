651fca36ff19450179a81228ce34c2f971a81f0c
Billy Laws <blaws05@gmail.com>
2025-12-22 14:43:13 +0000

    Windows: Implement _[w]sopen file APIs

--- Source/Windows/include/winternl.h
+++ Source/Windows/include/winternl.h
@@ -10,6 +10,18 @@
 extern "C" {
 #endif
 
+#define SH_COMPAT 0x00 /* Compatibility */
+#define SH_DENYRW 0x10 /* Deny read/write */
+#define SH_DENYWR 0x20 /* Deny write */
+#define SH_DENYRD 0x30 /* Deny read */
+#define SH_DENYNO 0x40 /* Deny nothing */
+
+#define _SH_COMPAT SH_COMPAT
+#define _SH_DENYRW SH_DENYRW
+#define _SH_DENYWR SH_DENYWR
+#define _SH_DENYRD SH_DENYRD
+#define _SH_DENYNO SH_DENYNO
+
 #define NtCurrentProcess() ((HANDLE) ~(ULONG_PTR)0)
 #define NtCurrentThread() ((HANDLE) ~(ULONG_PTR)1)
 
