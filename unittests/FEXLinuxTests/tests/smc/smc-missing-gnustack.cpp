6edf111ad2dab089180c3881c58a5fcbdabcaee5
Asaf Niv <svcx80@icloud.com>
2026-01-03 13:27:39 +0200

    unittests: fix formatting, change variable name

--- unittests/FEXLinuxTests/tests/smc/smc-missing-gnustack.cpp
+++ unittests/FEXLinuxTests/tests/smc/smc-missing-gnustack.cpp
@@ -5,12 +5,12 @@
 
 bool got_signal = false;
 
-static void sigsegv_handler(int signal, siginfo_t *siginfo, void* context) {
+static void sigsegv_handler(int signal, siginfo_t* siginfo, void* context) {
   REQUIRE(siginfo->si_code == SEGV_ACCERR);
   got_signal = true;
   size_t page_size = sysconf(_SC_PAGESIZE);
-  void *fault_addr = (void *)((uintptr_t)(siginfo->si_addr) & ~(page_size - 1));
-  REQUIRE(mprotect(fault_addr, page_size, PROT_READ | PROT_WRITE | PROT_EXEC) == 0);
+  void* fault_page = (void*)((uintptr_t)(siginfo->si_addr) & ~(page_size - 1));
+  REQUIRE(mprotect(fault_page, page_size, PROT_READ | PROT_WRITE | PROT_EXEC) == 0);
 }
 
 void register_signal_handler() {
@@ -41,8 +41,7 @@ TEST_CASE("smc-missing-gnustack: mmap other memory") {
   register_signal_handler();
   // Executing from other memory should fail on 64 bit but work on 32 bit
   size_t page_size = sysconf(_SC_PAGESIZE);
-  uint8_t *mem_code = static_cast<uint8_t *>(mmap(NULL, page_size, PROT_READ | PROT_WRITE,
-      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+  uint8_t* mem_code = static_cast<uint8_t*>(mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
   REQUIRE(mem_code != nullptr);
   *mem_code = 0xC3; // ret
   ((void (*)())(mem_code))();
