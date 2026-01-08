10d47585cd9fa7c03fbc42de2a9d4bb8a06dfa65
Jasmine Tang <jjasmine@igalia.com>
2026-01-08 21:51:07 +0000

    Rewrite to naive regex with dp

--- unittests/APITests/Regex.cpp
+++ unittests/APITests/Regex.cpp
@@ -5,29 +5,29 @@
 using namespace FEXCore::Utils;
 
 TEST_CASE("Singular regex") {
-  CHECK(Regex("a").matches("a") == true);
-  CHECK(Regex("a*").matches("aaaaaaa") == true);
+  CHECK(Regex::matches("a", "a") == true);
+  CHECK(Regex::matches("a*", "aaaaaaa") == true);
 }
 
 TEST_CASE("Concat regex") {
-  CHECK(Regex("aaa").matches("aaa") == true);
-  CHECK(Regex("ab").matches("ab") == true);
-  CHECK(Regex("a").matches("ab") == false);
-  CHECK(Regex("ab").matches("a") == false);
+  CHECK(Regex::matches("aaa", "aaa") == true);
+  CHECK(Regex::matches("ab", "ab") == true);
+  CHECK(Regex::matches("a", "ab") == false);
+  CHECK(Regex::matches("ab", "a") == false);
 }
 
-TEST_CASE("Dot regex") {
-  CHECK(Regex("*").matches("") == true);
-  CHECK(Regex("*").matches("setup.json") == true);
-  CHECK(Regex("setup.*").matches("setupjson") == false);
-  CHECK(Regex("setup*").matches("setup.json") == true);
-  CHECK(Regex("setup*").matches("setup/setup.json") == true);
-  CHECK(Regex("*setup*").matches("setup/setup.json") == true);
+TEST_CASE("Wildcard regex") {
+  CHECK(Regex::matches("*", "") == true);
+  CHECK(Regex::matches("*", "setup.json") == true);
+  CHECK(Regex::matches("setup.*", "setupjson") == false);
+  CHECK(Regex::matches("setup*", "setup.json") == true);
+  CHECK(Regex::matches("setup*", "setup/setup.json") == true);
+  CHECK(Regex::matches("*setup*", "setup/setup.json") == true);
 }
 
 
 // Tests potential usage inside fex itself
 TEST_CASE("FEX regex") {
-  CHECK(Regex("*Config*").matches("/home/ubuntu/.fex-emu/Config.json") == true);
-  CHECK(Regex("*Config.json").matches("/home/ubuntu/.fex-emu/Config.json") == true);
+  CHECK(Regex::matches("*Config*", "/home/ubuntu/.fex-emu/Config.json") == true);
+  CHECK(Regex::matches("*Config.json", "/home/ubuntu/.fex-emu/Config.json") == true);
 }
