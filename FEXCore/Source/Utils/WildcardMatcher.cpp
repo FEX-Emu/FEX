5ef559614bb204ddf4e9bcb993c684e4db725a7a
Jasmine Tang <jjasmine@igalia.com>
2026-01-20 18:49:05 +0000

    Addresses PR reviews

--- FEXCore/Source/Utils/WildcardMatcher.cpp
+++ FEXCore/Source/Utils/WildcardMatcher.cpp
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: MIT
 
-#include <FEXCore/Utils/Regex.h>
+#include <FEXCore/Utils/WildcardMatcher.h>
 #include "FEXCore/fextl/map.h"
-namespace FEXCore::Utils {
-class RegexMatcher {
+namespace FEXCore::Utils::Wildcard {
+class WildcardMatcher {
 private:
   std::string_view pattern;
   std::string_view text;
   fextl::map<std::pair<size_t, size_t>, bool> cache;
 
 public:
-  RegexMatcher(std::string_view pattern, std::string_view text)
-      : pattern(pattern), text(text) {}
+  WildcardMatcher(std::string_view pattern, std::string_view text)
+    : pattern(pattern)
+    , text(text) {}
 
   bool matchHelper(size_t p_idx, size_t t_idx);
 };
 
-bool RegexMatcher::matchHelper(size_t p_idx, size_t t_idx) {
+bool WildcardMatcher::matchHelper(size_t p_idx, size_t t_idx) {
   auto key = std::make_pair(p_idx, t_idx);
 
   // Check cache
@@ -30,24 +31,22 @@ bool RegexMatcher::matchHelper(size_t p_idx, size_t t_idx) {
   if (p_idx == pattern.size()) {
     result = (t_idx == text.size());
   }
-  // Wildcard 
+  // Wildcard
   else if (pattern[p_idx] == '*') {
     // Try matching zero characters, or one or more characters
-    result = matchHelper(p_idx + 1, t_idx) ||
-             (t_idx < text.size() && matchHelper(p_idx, t_idx + 1));
+    result = matchHelper(p_idx + 1, t_idx) || (t_idx < text.size() && matchHelper(p_idx, t_idx + 1));
   }
   // Match normally
   else {
-    result = (t_idx < text.size() && pattern[p_idx] == text[t_idx] &&
-              matchHelper(p_idx + 1, t_idx + 1));
+    result = (t_idx < text.size() && pattern[p_idx] == text[t_idx] && matchHelper(p_idx + 1, t_idx + 1));
   }
   cache[key] = result;
   return result;
 }
 
-bool Regex::matches(std::string_view pattern, std::string_view text) {
-  RegexMatcher matcher(pattern, text);
+bool Matches(std::string_view pattern, std::string_view text) {
+  WildcardMatcher matcher(pattern, text);
   return matcher.matchHelper(0, 0);
 }
 
-} // namespace FEXCore::Utils
+} // namespace FEXCore::Utils::Wildcard
