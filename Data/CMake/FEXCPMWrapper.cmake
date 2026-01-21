387440be723dfff5b33c9d816f0afeef7e170f62
crueter <crueter@eden-emu.dev>
2026-01-21 14:50:57 -0500

    horrible temp fix for catch2 stuff
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- Data/CMake/FEXCPMWrapper.cmake
+++ Data/CMake/FEXCPMWrapper.cmake
@@ -12,7 +12,7 @@
 
 set(CPM_SOURCE_CACHE ${PROJECT_SOURCE_DIR}/.cache/cpm)
 
-option(FEX_FORCE_BUNDLED OFF "Force the usage of bundled dependencies")
+option(FEX_FORCE_BUNDLED "Force the usage of bundled dependencies" OFF)
 
 include(CPM)
 include(Propagate)
