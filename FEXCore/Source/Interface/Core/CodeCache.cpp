44a6bfd8740f68c93df8121e03ccbbedaab8e42a
Tony Wasserka <neobrain@users.noreply.github.com>
2026-02-16 10:26:11 +0100

    Revert "CodeCache: Use defaulted dtor for ExecutableFileInfo"
    CodeCache.h has no dependency on SourcecodeResolver.h.
    
    This reverts commit f2bbc0eccda58d61bf44ee3f9a39cac61a3f75ca.

--- FEXCore/Source/Interface/Core/CodeCache.cpp
+++ FEXCore/Source/Interface/Core/CodeCache.cpp
@@ -11,6 +11,7 @@
 #include <Interface/IR/PassManager.h>
 
 #include <FEXCore/Core/Thunks.h>
+#include <FEXCore/HLE/SourcecodeResolver.h>
 #include <FEXCore/HLE/SyscallHandler.h>
 
 #include <FEXHeaderUtils/Filesystem.h>
@@ -29,6 +30,7 @@ ExecutableFileInfo::ExecutableFileInfo(fextl::unique_ptr<HLE::SourcecodeMap> Map
   , FileId(FileId)
   , Filename(Filename) {}
 #endif
+ExecutableFileInfo::~ExecutableFileInfo() = default;
 
 fextl::string CodeMap::GetBaseFilename(const ExecutableFileInfo& MainExecutable, bool AddNombSuffix) {
   auto FileId = MainExecutable.FileId;
