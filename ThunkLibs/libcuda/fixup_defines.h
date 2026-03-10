6fa27aac42ae59b985ed96093fed4a5604d3dee5
Ryan Houdek <Sonicadvance1@gmail.com>
2026-03-10 12:42:08 -0700

    Thunks: Implement support for CUDA
    This is enough to get less complex cuda applications running, and is a
    good starting spot to slowly finish off the remaining implementation.
    
    Some information:
    - 429 functions in total
    - 153 only compiled for 64-bit (35.6%)
    - 7 functions disabled entirely (1.6%)
    
    The main thing /not/ working with this initial implementation is .cu
    files compiled in to an ELF using the static cuda runtime. This is due
    to the `cuGetExportTable` function being stubbed out and the static cuda
    RT requires at least two interfaces from that function before it
    continues.
    
    This function isn't publicly documented by NVIDIA but has been publicly
    reverse engineered to be fairly trivial. It's just a jump table with the
    first element being the size of the table in bytes.
    
    That will be the next step of the implementation.

---
+++ ThunkLibs/libcuda/fixup_defines.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+#pragma once
+#include <cstddef>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*CUhostFn)(void*);
+enum cudaError_enum {};
+enum CUgreenCtxCreate_flags {};
+enum CUdevResourceType {};
+
+#ifdef __cplusplus
+}
+#endif
