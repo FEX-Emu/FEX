dd0702d30f3f49c8d5bfa24510c9544f1166837e
Billy Laws <blaws05@gmail.com>
2026-04-28 23:53:12 +0000

    unittests/ASM: Mark SSE4a/CLZERO as required for tests using them

--- unittests/ASM/SSE4a/insertq_variable.asm
+++ unittests/ASM/SSE4a/insertq_variable.asm
@@ -9,7 +9,8 @@
     "XMM5": ["0x0acb4bcc689712b0", "0", "0x1c86432298df55c8", "0xb29bfeda891be9cc"],
     "XMM6": ["0x1c8625a6a7a855c8", "0", "0x88b0bd28710f2147", "0xc4e95e887fb5ac38"],
     "XMM7": ["0x88b3b4b5b6b7b847", "0", "0xa7df8c2ad03e5be4", "0x6c70d1eec2d395ea"]
-  }
+  },
+  "HostFeatures": ["SSE4A"]
 }
 %endif
 
