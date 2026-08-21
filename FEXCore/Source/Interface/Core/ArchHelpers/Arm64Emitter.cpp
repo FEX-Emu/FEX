dfcbdac34749b10cc5957c8aab1bbe8541a1e6cf
Ryan Houdek <Sonicadvance1@gmail.com>
2026-08-21 14:45:33 -0700

    JIT: Fixes some alignment logic
    Noticed while taking a look at the relocations that we were technically
    not doing alignment before writing down code size.
    
    - Make sure Align16B isn't used with unaligned code with assert
    - Switch an `Align` over to `Align(16)` to force 16-byte alignment
      - Without NOP insertion, as this is data at this point, so just zeros.
    - Record data size after that alignment
    - Remove the `Align16B` that occurred afterwards
      - Previous query between alignments would leave us with up to 12 bytes
        unaccounted for.
    - Ensure everything is using the correct sizes by not querying again
    - Ensure that emission buffer abuse can't happen by zeroing the buffer.

--- FEXCore/Source/Interface/Core/ArchHelpers/Arm64Emitter.cpp
+++ FEXCore/Source/Interface/Core/ArchHelpers/Arm64Emitter.cpp
@@ -1123,6 +1123,7 @@ void Arm64Emitter::FillForPreserveAllABICall(bool FPRs) {
 
 void Arm64Emitter::Align16B() {
   uint64_t CurrentOffset = GetCursorAddress<uint64_t>();
+  LOGMAN_THROW_A_FMT((CurrentOffset & 3) == 0, "Can't Align16B code that isn't 4-byte aligned!");
   for (uint64_t i = (-CurrentOffset & 0xF); i != 0; i -= 4) {
     nop();
   }
